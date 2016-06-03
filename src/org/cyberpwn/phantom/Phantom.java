3ab1b613ae47d6b6f6961c39f3b4011b11419543
Daniel Mills <danielmillst@gmail.com>
2016-06-03 05:00:22 -0400

    Register & Handle Test controller

--- src/org/cyberpwn/phantom/Phantom.java
+++ src/org/cyberpwn/phantom/Phantom.java
@@ -1,21 +1,49 @@
 package org.cyberpwn.phantom;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.cyberpwn.phantom.construct.PhantomPlugin;
-import org.cyberpwn.phantom.test.TestControllerA;
+import org.cyberpwn.phantom.test.TestController;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class Phantom extends PhantomPlugin
 {
-	private TestControllerA ta;
+	private TestController testController;
 	
 	public void enable()
 	{
-		ta = new TestControllerA(this);
-		
-		register(ta);
+		testController = new TestController(this);
+		register(testController);
 	}
 	
 	public void disable()
 	{
 		
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		if(command.getName().equals("phantom"))
+		{
+			if(args.length > 0)
+			{
+				if(args[0].equalsIgnoreCase("test") || args[0].equalsIgnoreCase("t"))
+				{
+					if(args.length == 2)
+					{
+						testController.execute(sender, args[1]);
+					}
+					
+					else
+					{
+						sender.sendMessage(ChatColor.GREEN + testController.getTests().k().toString(", "));
+					}
+				}
+			}
+		}
+		
+		return false;
 	}
 }
