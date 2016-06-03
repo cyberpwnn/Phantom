a9a4881c5f52bac76cbd75c217b53b1d4025919d
Daniel Mills <danielmillst@gmail.com>
2016-06-03 04:59:40 -0400

    Yaml Data Ports for Clust

---
+++ src/org/cyberpwn/clust/ext/YAMLDataOutput.java
@@ -0,0 +1,39 @@
+package org.cyberpwn.clust.ext;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.cyberpwn.clust.DataCluster;
+import org.cyberpwn.clust.DataOutput;
+
+public class YAMLDataOutput extends DataOutput
+{
+	@Override
+	public void save(DataCluster cluster, File file) throws IOException
+	{
+		super.save(cluster, file);
+		
+		FileConfiguration fc = new YamlConfiguration();
+		
+		try
+		{
+			fc.load(file);
+		}
+		
+		catch(InvalidConfigurationException e)
+		{
+			file.delete();
+			file.createNewFile();
+		}
+		
+		for(String i : cluster.getData().keySet())
+		{
+			fc.set(i, cluster.getAbstract(i));
+		}
+		
+		fc.save(file);
+	}
+}
