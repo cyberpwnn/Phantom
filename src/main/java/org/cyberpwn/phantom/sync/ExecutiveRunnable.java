05cf4816bd31671f27f1903fd6bf190692e9a2f6
Daniel Mills <danielmillst@gmail.com>
2016-07-10 22:39:30 -0400

    Allow cancels and repeats

--- src/main/java/org/cyberpwn/phantom/sync/ExecutiveRunnable.java
+++ src/main/java/org/cyberpwn/phantom/sync/ExecutiveRunnable.java
@@ -3,10 +3,14 @@ package org.cyberpwn.phantom.sync;
 public class ExecutiveRunnable<T> implements Runnable
 {
 	private T next;
+	private Boolean cancelled;
+	private Boolean repeated;
 	
 	public void run(T next)
 	{
 		this.next = next;
+		this.cancelled = false;
+		this.repeated = false;
 	}
 	
 	@Override
@@ -18,5 +22,25 @@ public class ExecutiveRunnable<T> implements Runnable
 	public T next()
 	{
 		return next;
+	}
+	
+	public void cancel()
+	{
+		this.cancelled = true;
+	}
+	
+	public boolean isCancelled()
+	{
+		return cancelled;
+	}
+	
+	public void repeat()
+	{
+		this.repeated = true;
+	}
+	
+	public boolean isRepeated()
+	{
+		return repeated;
 	}
 }
