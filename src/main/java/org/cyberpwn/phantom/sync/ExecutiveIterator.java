b30c4d9a4c20999f3cc74349a0fda94bd58444bc
Daniel Mills <danielmillst@gmail.com>
2016-07-10 22:39:11 -0400

    allow repeats

--- src/main/java/org/cyberpwn/phantom/sync/ExecutiveIterator.java
+++ src/main/java/org/cyberpwn/phantom/sync/ExecutiveIterator.java
@@ -7,30 +7,62 @@ public class ExecutiveIterator<T> implements Iterator<T>
 {
 	private Iterator<T> it;
 	private ExecutiveRunnable<T> runnable;
+	private Boolean cancelled;
+	private T repeated;
 	
 	public ExecutiveIterator(Iterator<T> it, ExecutiveRunnable<T> runnable)
 	{
 		this.it = it;
 		this.runnable = runnable;
+		this.cancelled = false;
+		this.repeated = null;
 	}
 	
 	public ExecutiveIterator(List<T> it, ExecutiveRunnable<T> runnable)
 	{
 		this.it = it.iterator();
 		this.runnable = runnable;
+		this.cancelled = false;
+		this.repeated = null;
 	}
 
 	@Override
 	public boolean hasNext()
 	{
-		return it.hasNext();
+		return it.hasNext() && !cancelled;
 	}
 
 	@Override
 	public T next()
 	{
-		T t = it.next();
+		T t = null;
+		
+		if(repeated != null)
+		{
+			t = repeated;
+		}
+		
+		else
+		{
+			t = it.next();
+		}
+		
 		runnable.run(t);
+	
+		if(runnable.isCancelled())
+		{
+			cancelled = true;	
+		}
+		
+		if(runnable.isRepeated())
+		{
+			repeated = t;
+		}
+		
+		else
+		{
+			repeated = null;
+		}
 		
 		return t;
 	}
