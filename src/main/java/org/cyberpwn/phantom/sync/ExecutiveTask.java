e8d56d1c91223fc03fb8f8258f9e59f726e3dfc1
Daniel Mills <danielmillst@gmail.com>
2016-07-10 22:39:21 -0400

    ExecTasks

--- src/main/java/org/cyberpwn/phantom/sync/ExecutiveTask.java
+++ src/main/java/org/cyberpwn/phantom/sync/ExecutiveTask.java
@@ -1,9 +1,50 @@
 package org.cyberpwn.phantom.sync;
 
+import org.cyberpwn.phantom.Phantom;
+import org.cyberpwn.phantom.util.M;
+
 public class ExecutiveTask<T>
 {
-	public ExecutiveTask(ExecutiveTask<T> it, Double limit, Integer interval, Runnable finish)
+	private ExecutiveIterator<T> it;
+	private Double limit;
+	private Integer interval;
+	private Runnable finish;
+	private Task task;
+	
+	public ExecutiveTask(ExecutiveIterator<T> it, Double limit, Integer interval, Runnable finish)
 	{
+		this.it = it;
+		this.limit = limit;
+		this.interval = interval;
+		this.finish = finish;
 		
+		this.task = new Task(Phantom.instance(), ExecutiveTask.this.interval)
+		{
+			public void run()
+			{
+				long ns = M.ns();
+				
+				while(ExecutiveTask.this.it.hasNext() && ((double)(M.ms() - ns) / 1000000.0) < ExecutiveTask.this.limit)
+				{
+					ExecutiveTask.this.it.next();
+				}
+				
+				if(!ExecutiveTask.this.it.hasNext())
+				{
+					ExecutiveTask.this.finish.run();
+					cancel();
+				}
+			}
+		};
+	}
+	
+	public void cancel()
+	{
+		task.cancel();
+	}
+	
+	public boolean isRunning()
+	{
+		return task.isRunning();
 	}
 }
