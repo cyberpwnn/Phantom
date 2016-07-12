a48fe8a7145d4eb19cece0fe210f5e51d224abd5
Daniel Mills <danielmillst@gmail.com>
2016-07-12 12:50:18 -0400

    Dimensions

---
+++ src/main/java/org/cyberpwn/phantom/world/Dimension.java
@@ -0,0 +1,30 @@
+package org.cyberpwn.phantom.world;
+
+public class Dimension
+{
+	private final int width;
+	private final int height;
+	private final int depth;
+	
+	public Dimension(int width, int height, int depth)
+	{
+		this.width = width;
+		this.height = height;
+		this.depth = depth;
+	}
+	
+	public int getWidth()
+	{
+		return width;
+	}
+	
+	public int getHeight()
+	{
+		return height;
+	}
+	
+	public int getDepth()
+	{
+		return depth;
+	}
+}
