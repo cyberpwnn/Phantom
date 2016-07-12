45d63dd0fca79fdb9b0f7c28631f5ad85da07e1b
Daniel Mills <danielmillst@gmail.com>
2016-07-12 12:50:11 -0400

    generic categories

---
+++ src/main/java/org/cyberpwn/phantom/lang/Heaft.java
@@ -0,0 +1,6 @@
+package org.cyberpwn.phantom.lang;
+
+public enum Heaft
+{
+	TINY, SMALL, NORMAL, BIG, MASSIVE
+}
---
+++ src/main/java/org/cyberpwn/phantom/lang/Priority.java
@@ -0,0 +1,6 @@
+package org.cyberpwn.phantom.lang;
+
+public enum Priority
+{
+	LOWEST, VERY_LOW, LOW, NORMAL, HIGH, VERY_HIGH, HIGHEST;
+}
