872018fc876864cfe78dadddc059d9be0b542f35
Daniel Mills <danielmillst@gmail.com>
2016-07-13 03:35:52 -0400

    Invalid Region Exception

---
+++ src/main/java/org/cyberpwn/phantom/game/MapInvalidRegionAddException.java
@@ -0,0 +1,11 @@
+package org.cyberpwn.phantom.game;
+
+public class MapInvalidRegionAddException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+	
+	public MapInvalidRegionAddException(String message)
+	{
+		super(message);
+	}
+}
