7024b2e09beeecd542257d2072db7a7d109ec8de
Daniel Mills <danielmillst@gmail.com>
2016-07-14 10:41:12 -0400

    Average util

---
+++ src/main/java/org/cyberpwn/phantom/util/Average.java
@@ -0,0 +1,54 @@
+package org.cyberpwn.phantom.util;
+
+import org.cyberpwn.phantom.lang.GList;
+
+public class Average
+{
+	private int limit;
+	private GList<Double> data;
+	private double average;
+	
+	public Average(int limit)
+	{
+		this.limit = limit;
+		this.data = new GList<Double>();
+		this.average = 0.0;
+	}
+	
+	public void put(double d)
+	{
+		data.add(d);
+		
+		if(limit > 0)
+		{
+			M.lim(data, limit);
+		}
+		
+		average = M.avg(data);
+	}
+	
+	public int getLimit()
+	{
+		return limit;
+	}
+	
+	public void setLimit(int limit)
+	{
+		this.limit = limit;
+	}
+	
+	public GList<Double> getData()
+	{
+		return data;
+	}
+	
+	public void setData(GList<Double> data)
+	{
+		this.data = data;
+	}
+	
+	public double getAverage()
+	{
+		return average;
+	}
+}
