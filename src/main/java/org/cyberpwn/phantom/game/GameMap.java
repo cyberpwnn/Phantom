ed927b99b75f2d05963daccedef72fe8d7badc19
Daniel Mills <danielmillst@gmail.com>
2016-07-14 11:32:51 -0400

    Game map getters

--- src/main/java/org/cyberpwn/phantom/game/GameMap.java
+++ src/main/java/org/cyberpwn/phantom/game/GameMap.java
@@ -1,8 +1,11 @@
 package org.cyberpwn.phantom.game;
 
 import org.bukkit.Location;
+import org.bukkit.World;
 
 public interface GameMap<M, G, T, P>
 {
 	public boolean contains(Location location);
+	public World getWorld();
+	public G getGame();
 }
