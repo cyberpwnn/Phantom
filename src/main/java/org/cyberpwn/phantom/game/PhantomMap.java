fbc351b2805d42a9b73772ff1e8f7dbe0ea82e27
Daniel Mills <danielmillst@gmail.com>
2016-07-14 11:33:11 -0400

    Phantom map impl

--- src/main/java/org/cyberpwn/phantom/game/PhantomMap.java
+++ src/main/java/org/cyberpwn/phantom/game/PhantomMap.java
@@ -1,17 +1,34 @@
 package org.cyberpwn.phantom.game;
 
 import org.bukkit.Location;
+import org.bukkit.World;
 
 public class PhantomMap<M extends GameMap<M, G, T, P>, G extends Game<M, G, T, P>, T extends Team<M, G, T, P>, P extends GamePlayer<M, G, T, P>> implements GameMap<M, G, T, P>
 {
-	public PhantomMap()
+	protected World world;
+	protected G game;
+	
+	public PhantomMap(G game, World world)
 	{
-		
+		this.world = world;
+		this.game = game;
 	}
 	
 	@Override
 	public boolean contains(Location location)
 	{
 		return false;
+	}
+
+	@Override
+	public World getWorld()
+	{
+		return world;
+	}
+
+	@Override
+	public G getGame()
+	{
+		return game;
 	}
 }
