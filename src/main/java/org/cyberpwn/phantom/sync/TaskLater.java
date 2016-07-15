8df870ec7d474a2085492cd880fd3df209f689fc
Daniel Mills <danielmillst@gmail.com>
2016-07-15 02:00:37 -0400

    Task later util

---
+++ src/main/java/org/cyberpwn/phantom/sync/TaskLater.java
@@ -0,0 +1,27 @@
+package org.cyberpwn.phantom.sync;
+
+import org.cyberpwn.phantom.Phantom;
+import org.cyberpwn.phantom.construct.Controllable;
+
+public class TaskLater implements Runnable
+{
+	private Controllable pl;
+	
+	public TaskLater(Controllable pl)
+	{
+		this.pl = pl;
+		pl.getPlugin().scheduleSyncTask(0, this);
+	}
+	
+	public TaskLater()
+	{
+		this.pl = Phantom.instance();
+		pl.getPlugin().scheduleSyncTask(0, this);
+	}
+	
+	@Override
+	public void run()
+	{
+		
+	}
+}
