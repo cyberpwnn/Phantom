d6166c5737d383d3b6c262343ae04b5a718ea88f
Daniel Mills <danielmillst@gmail.com>
2016-07-15 02:00:53 -0400

    GameAdapters

--- src/main/java/org/cyberpwn/phantom/game/GameAdapter.java
+++ src/main/java/org/cyberpwn/phantom/game/GameAdapter.java
@@ -1,8 +1,10 @@
 package org.cyberpwn.phantom.game;
 
+import org.bukkit.entity.Player;
 import org.cyberpwn.phantom.lang.Alphabet;
 
-public interface GameAdapter
+public interface GameAdapter<M extends GameMap<M, G, T, P>, G extends Game<M, G, T, P>, T extends Team<M, G, T, P>, P extends GamePlayer<M, G, T, P>>
 {
-	public void onGameCreate(Alphabet alphabet);
+	public G onGameCreate(Alphabet alphabet);
+	public P onPlayerObjectCreate(Player p);
 }
