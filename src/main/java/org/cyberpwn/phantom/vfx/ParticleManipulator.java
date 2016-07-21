c809484388a4ecd09cf63e9205913de39e54a9e3
Daniel Mills <danielmillst@gmail.com>
2016-07-21 19:12:39 -0400

    Particle Manipulators

---
+++ src/main/java/org/cyberpwn/phantom/vfx/ParticleManipulator.java
@@ -0,0 +1,12 @@
+package org.cyberpwn.phantom.vfx;
+
+/**
+ * Base implementation for a particle manipluator
+ * 
+ * @author cyberpwn
+ *
+ */
+public class ParticleManipulator extends PhantomEffect
+{
+	
+}
