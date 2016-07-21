2e8d8f2c4c1cd2e2f61809b154ae909521fdd43d
Daniel Mills <danielmillst@gmail.com>
2016-07-21 01:18:46 -0400

    Remove old game api

--- src/test/java/org/cyberpwn/phantom/game/MinigamePlugin.java
+++ src/test/java/org/cyberpwn/phantom/game/MinigamePlugin.java
@@ -5,18 +5,9 @@ import org.cyberpwn.phantom.construct.PhantomPlugin;
 //This is the plugin file, this will be called on start
 public class MinigamePlugin extends PhantomPlugin
 {
-	//Define the GameController with the construct api.
-	private ExampleGameController controller;
-	
 	//Use ENABLE not onEnable. This sets up the controllers
 	public void enable()
 	{
-		//Initialize the controller as this plugin being the parent
-		controller = new ExampleGameController(this);
 		
-		//Register the controller so it maps its 
-		// - START <> Plugin Start
-		// - STOP <> Plugin Stop
-		register(controller);
 	}
 }
