bdc20997f07e19db26a4cd1710b7149d7942e834
Daniel Mills <danielmillst@gmail.com>
2016-07-24 00:54:08 -0400

    Priority topdowns

--- src/main/java/org/cyberpwn/phantom/lang/Priority.java
+++ src/main/java/org/cyberpwn/phantom/lang/Priority.java
@@ -3,4 +3,9 @@ package org.cyberpwn.phantom.lang;
 public enum Priority
 {
 	LOWEST, VERY_LOW, LOW, NORMAL, HIGH, VERY_HIGH, HIGHEST;
+	
+	public static GList<Priority> topDown()
+	{
+		return new GList<Priority>().qadd(HIGHEST).qadd(VERY_HIGH).qadd(HIGH).qadd(NORMAL).qadd(LOW).qadd(VERY_LOW).qadd(LOWEST);
+	}
 }
