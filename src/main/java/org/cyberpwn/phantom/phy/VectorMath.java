a306f9db7f2302da8f934caae9350c1b6a9241df
Daniel Mills <danielmillst@gmail.com>
2016-07-25 17:51:53 -0400

    Vector math

---
+++ src/main/java/org/cyberpwn/phantom/phy/VectorMath.java
@@ -0,0 +1,57 @@
+package org.cyberpwn.phantom.phy;
+
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+/**
+ * Vector utilities
+ * 
+ * @author cyberpwn
+ *
+ */
+public class VectorMath
+{
+	/**
+	 * Get a normalized vector going from a location to another
+	 * 
+	 * @param from
+	 *            from here
+	 * @param to
+	 *            to here
+	 * @return the normalized vector direction
+	 */
+	public static Vector direction(Location from, Location to)
+	{
+		return to.subtract(from).toVector().normalize();
+	}
+	
+	/**
+	 * Reverse a direction
+	 * 
+	 * @param v
+	 *            the direction
+	 * @return the reversed direction
+	 */
+	public static Vector reverse(Vector v)
+	{
+		v.setX(-v.getX());
+		v.setY(-v.getY());
+		v.setZ(-v.getZ());
+		return v;
+	}
+	
+	/**
+	 * Get a speed value from a vector (velocity)
+	 * 
+	 * @param v
+	 *            the vector
+	 * @return the speed
+	 */
+	public static double getSpeed(Vector v)
+	{
+		Vector vi = new Vector(0, 0, 0);
+		Vector vt = new Vector(0, 0, 0).add(v);
+		
+		return vi.distance(vt);
+	}
+}
