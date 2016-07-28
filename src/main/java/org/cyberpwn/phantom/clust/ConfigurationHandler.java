686bf9d3ce294730caf4c85c0097817e9a81fa9b
Daniel Mills <danielmillst@gmail.com>
2016-07-28 00:09:41 -0400

    Config utils mysql

--- src/main/java/org/cyberpwn/phantom/clust/ConfigurationHandler.java
+++ src/main/java/org/cyberpwn/phantom/clust/ConfigurationHandler.java
@@ -4,18 +4,33 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.cyberpwn.phantom.lang.GList;
 import org.cyberpwn.phantom.util.D;
 
 /**
+ * The heart of dataclusters. Most should not have to use this. It is
+ * essentially a utility class for controllers and any other components of
+ * Phantom to use automatically when you need it called. For example,
+ * loadCluster would call parts of this when needed.
  * 
  * @author cyberpwn
  *
  */
 public class ConfigurationHandler
 {
+	/**
+	 * Read data from config files and dataclusters and put them into the keyed
+	 * config fields in the configurable class supplied
+	 * 
+	 * @param c
+	 *            the configurable object
+	 */
 	public static void toFields(Configurable c)
 	{
 		for(Field i : c.getClass().getDeclaredFields())
@@ -76,6 +91,13 @@ public class ConfigurationHandler
 		}
 	}
 	
+	/**
+	 * Write data to the cluster from the keyed fields from the configurable
+	 * object
+	 * 
+	 * @param c
+	 *            the configurable object
+	 */
 	public static void fromFields(Configurable c)
 	{
 		for(Field i : c.getClass().getDeclaredFields())
@@ -124,6 +146,13 @@ public class ConfigurationHandler
 		}
 	}
 	
+	/**
+	 * But is this type valid?
+	 * 
+	 * @param type
+	 *            the class type
+	 * @return true if can be saved
+	 */
 	public static boolean isValidType(Class<?> type)
 	{
 		if(type.equals(String.class))
@@ -251,5 +280,164 @@ public class ConfigurationHandler
 		
 		fromFields(c);
 		new YAMLDataOutput().save(c.getConfiguration(), config);
+	}
+	
+	/**
+	 * Handle reading in configs. Also adds new paths that do not exist in the
+	 * file from the onNewConfig(), and adds default values
+	 * 
+	 * @param c
+	 *            the configurable object
+	 * @throws IOException
+	 *             1337
+	 * @throws SQLException
+	 *             shit happens
+	 * @throws ClassNotFoundException
+	 *             really bad shit happens
+	 */
+	public static void readMySQL(Configurable c, DatabaseConnection connection) throws IOException, ClassNotFoundException, SQLException
+	{
+		fromFields(c);
+		c.onNewConfig();
+		fromMysql(c, connection);
+		toMysql(c, connection);
+		toFields(c);
+		c.onReadConfig();
+	}
+	
+	/**
+	 * Handle saving configs
+	 * 
+	 * @param c
+	 *            the configurable object
+	 * @throws IOException
+	 *             1337
+	 * @throws SQLException
+	 *             shit happens
+	 * @throws ClassNotFoundException
+	 *             really bad shit happens
+	 */
+	public static void saveMySQL(Configurable c, DatabaseConnection connection) throws IOException, ClassNotFoundException, SQLException
+	{
+		fromFields(c);
+		toMysql(c, connection);
+	}
+	
+	/**
+	 * Checks if the configurable object has a table annotation for SQL purposes
+	 * 
+	 * @param c
+	 *            the configurable object
+	 * @return true if a table is defined
+	 */
+	public static boolean hasTable(Configurable c)
+	{
+		return c.getClass().isAnnotationPresent(Tabled.class);
+	}
+	
+	/**
+	 * Get the table defined in this tabled configurable object
+	 * 
+	 * @param c
+	 *            the tabled configurable object
+	 * @return the table
+	 */
+	public static String getTable(Configurable c)
+	{
+		return c.getClass().getAnnotation(Tabled.class).value();
+	}
+	
+	/**
+	 * Pull data from mysql into the configurable object
+	 * 
+	 * @param c
+	 *            the configurable object
+	 * @param connection
+	 *            the connection data
+	 * @throws SQLException
+	 *             shit happens
+	 * @throws ClassNotFoundException
+	 *             really bad shit happens
+	 */
+	public static void fromMysql(Configurable c, DatabaseConnection connection) throws SQLException, ClassNotFoundException
+	{
+		MySQL db = new MySQL(connection.getAddress(), String.valueOf(connection.getPort()), connection.getDatabase(), connection.getUsername(), connection.getPassword());
+		Connection conn = db.openConnection();
+		PreparedStatement s = conn.prepareStatement("CREATE TABLE IF NOT EXISTS `?` (`key` varchar(MAX), `phad` varchar(MAX));");
+		s.setString(1, getTable(c));
+		s.execute();
+		s.close();
+		
+		PreparedStatement st = conn.prepareStatement("SELECT * FROM `?` WHERE key=?;");
+		st.setString(1, getTable(c));
+		st.setString(2, c.getCodeName());
+		ResultSet res = st.executeQuery();
+		
+		if(res.next())
+		{
+			PreparedStatement stx = conn.prepareStatement("SELECT `phad` FROM `?` WHERE key=?;");
+			stx.setString(1, getTable(c));
+			stx.setString(2, c.getCodeName());
+			ResultSet resx = stx.executeQuery();
+			JSONObject jso = new JSONObject(resx.getString("phad"));
+			c.getConfiguration().addJson(jso);
+			resx.close();
+			stx.close();
+		}
+		
+		res.close();
+		st.close();
+		db.closeConnection();
+	}
+	
+	/**
+	 * Put data from configurable objects into mysql
+	 * 
+	 * @param c
+	 *            the configurable object
+	 * @param connection
+	 *            the connection data
+	 * @throws SQLException
+	 *             shit happens
+	 * @throws ClassNotFoundException
+	 *             really bad shit happens
+	 */
+	public static void toMysql(Configurable c, DatabaseConnection connection) throws SQLException, ClassNotFoundException
+	{
+		MySQL db = new MySQL(connection.getAddress(), String.valueOf(connection.getPort()), connection.getDatabase(), connection.getUsername(), connection.getPassword());
+		Connection conn = db.openConnection();
+		PreparedStatement s = conn.prepareStatement("CREATE TABLE IF NOT EXISTS `?` (`key` varchar(MAX), `phad` varchar(MAX));");
+		s.setString(1, getTable(c));
+		s.execute();
+		s.close();
+		
+		PreparedStatement st = conn.prepareStatement("SELECT * FROM `?` WHERE key=?;");
+		st.setString(1, getTable(c));
+		st.setString(2, c.getCodeName());
+		ResultSet res = st.executeQuery();
+		
+		if(res.next())
+		{
+			PreparedStatement stx = conn.prepareStatement("UPDATE `?` SET phad=? WHERE key=?;");
+			stx.setString(1, getTable(c));
+			stx.setString(2, c.getConfiguration().toJSON().toString());
+			stx.setString(3, c.getCodeName());
+			stx.executeUpdate();
+			stx.close();
+		}
+		
+		else
+		{
+			PreparedStatement stx = conn.prepareStatement("INSERT INTO `?` values(?,?);");
+			stx.setString(1, getTable(c));
+			stx.setString(2, c.getCodeName());
+			stx.setString(3, c.getConfiguration().toJSON().toString());
+			stx.executeUpdate();
+			stx.close();
+		}
+		
+		res.close();
+		st.close();
+		db.closeConnection();
 	}
 }
