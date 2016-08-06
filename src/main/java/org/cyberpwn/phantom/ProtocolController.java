3519ce3c4e87fb4566ae6b06ef49486a196658ee
Daniel Mills <danielmillst@gmail.com>
2016-08-05 20:15:23 -0400

    Protocol lib handles

---
+++ src/main/java/org/cyberpwn/phantom/ProtocolController.java
@@ -0,0 +1,26 @@
+package org.cyberpwn.phantom;
+
+import org.cyberpwn.phantom.construct.Controllable;
+import org.cyberpwn.phantom.construct.Controller;
+import org.cyberpwn.phantom.nms.EntityHider;
+import org.cyberpwn.phantom.nms.EntityHider.Policy;
+
+public class ProtocolController extends Controller
+{
+	private EntityHider entityHider;
+	
+	public ProtocolController(Controllable parentController)
+	{
+		super(parentController);
+	}
+	
+	public void onStart()
+	{
+		this.entityHider = new EntityHider(getPlugin(), Policy.BLACKLIST);
+	}
+	
+	public EntityHider getHider()
+	{
+		return entityHider;
+	}
+}
