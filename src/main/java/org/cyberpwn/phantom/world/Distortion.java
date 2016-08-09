fc976fdc0e9dd90b5d67a2d9b04a3b99f4a69546
Daniel Mills <danielmillst@gmail.com>
2016-08-08 17:32:21 -0700

    A Distortion

---
+++ src/main/java/org/cyberpwn/phantom/world/Distortion.java
@@ -0,0 +1,12 @@
+package org.cyberpwn.phantom.world;
+
+public interface Distortion
+{
+	/**
+	 * Distorts the schematic
+	 * 
+	 * @param s
+	 *            the schematic to distort
+	 */
+	public void onDistort(Schematic s);
+}
