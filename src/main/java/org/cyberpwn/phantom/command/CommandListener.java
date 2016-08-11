a3d361ef40892d433c081bd047e0048f93a7208e
Daniel Mills <danielmillst@gmail.com>
2016-08-11 04:10:59 -0700

    Command listeners for controllers

---
+++ src/main/java/org/cyberpwn/phantom/command/CommandListener.java
@@ -0,0 +1,37 @@
+package org.cyberpwn.phantom.command;
+
+import org.cyberpwn.phantom.lang.GList;
+
+/**
+ * A Basic command listener
+ * 
+ * @author cyberpwn
+ */
+public interface CommandListener extends CommandMessenger
+{
+	/**
+	 * A new command has been called
+	 * 
+	 * @param sender
+	 *            the sender
+	 * @param command
+	 *            the command
+	 * @return returns true if the command was handled. Returns false if the
+	 *         command was not handled
+	 */
+	public boolean onCommand(PhantomCommandSender sender, PhantomCommand command);
+	
+	/**
+	 * Get the command name
+	 * 
+	 * @return the name
+	 */
+	public String getCommandName();
+	
+	/**
+	 * Get the command alias names
+	 * 
+	 * @return the names
+	 */
+	public GList<String> getCommandAliases();
+}
