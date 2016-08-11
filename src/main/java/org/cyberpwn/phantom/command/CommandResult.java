21c70eff8a50165dfd0ebc1589f1462d49c279c5
Daniel Mills <danielmillst@gmail.com>
2016-08-11 04:09:05 -0700

    Command results

---
+++ src/main/java/org/cyberpwn/phantom/command/CommandResult.java
@@ -0,0 +1,24 @@
+package org.cyberpwn.phantom.command;
+
+/**
+ * Command result
+ * 
+ * @author cyberpwn
+ */
+public enum CommandResult
+{
+	/**
+	 * Command was handled
+	 */
+	HANDLED,
+	
+	/**
+	 * No handle
+	 */
+	NO_HANDLE,
+	
+	/**
+	 * Exception
+	 */
+	EXCEPTION;
+}
