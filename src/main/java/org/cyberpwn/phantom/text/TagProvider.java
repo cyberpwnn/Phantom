15de9dc95f4f94da11f3530df0e0100afb0f3410
Daniel Mills <danielmillst@gmail.com>
2016-08-11 04:09:49 -0700

    Tag providers

---
+++ src/main/java/org/cyberpwn/phantom/text/TagProvider.java
@@ -0,0 +1,23 @@
+package org.cyberpwn.phantom.text;
+
+/**
+ * Provides chat tags
+ * 
+ * @author cyberpwn
+ */
+public interface TagProvider
+{
+	/**
+	 * Get the chat tag for this object
+	 * 
+	 * @return the object
+	 */
+	public String getChatTag();
+	
+	/**
+	 * Get the chat hover tag for this object
+	 * 
+	 * @return the object
+	 */
+	public String getChatTagHover();
+}
