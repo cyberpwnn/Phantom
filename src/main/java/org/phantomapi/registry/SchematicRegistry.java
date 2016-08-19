be5689540e8a09e611abc15a51bba38736deb320
Daniel Mills <danielmillst@gmail.com>
2016-08-19 01:59:05 -0400

    Schematic registry

---
+++ src/main/java/org/phantomapi/registry/SchematicRegistry.java
@@ -0,0 +1,13 @@
+package org.phantomapi.registry;
+
+import org.phantomapi.world.Schematic;
+
+/**
+ * A schematic registry
+ * 
+ * @author cyberpwn
+ */
+public class SchematicRegistry extends RegistryBank<Schematic>
+{
+	
+}
