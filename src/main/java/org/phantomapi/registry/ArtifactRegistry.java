f28e07f638b59271893f940a991de381e9f665ff
Daniel Mills <danielmillst@gmail.com>
2016-08-19 01:58:15 -0400

    Artifact registry

---
+++ src/main/java/org/phantomapi/registry/ArtifactRegistry.java
@@ -0,0 +1,13 @@
+package org.phantomapi.registry;
+
+import org.phantomapi.world.Artifact;
+
+/**
+ * An artifact registry
+ * 
+ * @author cyberpwn
+ */
+public class ArtifactRegistry extends RegistryBank<Artifact>
+{
+	
+}
