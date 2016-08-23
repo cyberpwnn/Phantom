6e2eaa905247dcf35be4876088ce9b32efd0f2fd
Daniel Mills <danielmillst@gmail.com>
2016-08-22 20:11:37 -0400

    Basic placeholders

---
+++ src/main/java/org/phantomapi/placeholder/PlaceholderHook.java
@@ -0,0 +1,44 @@
+package org.phantomapi.placeholder;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.phantomapi.Phantom;
+import org.phantomapi.nms.NMSX;
+import me.clip.placeholderapi.external.EZPlaceholderHook;
+
+public class PlaceholderHook extends EZPlaceholderHook
+{
+	public PlaceholderHook(Plugin plugin, String placeholderName)
+	{
+		super(plugin, placeholderName);
+	}
+
+	@Override
+	public String onPlaceholderRequest(Player p, String q)
+	{
+		if(q.equalsIgnoreCase("server_count"))
+		{
+			return Phantom.instance().onlinePlayers().size() + "";
+		}
+		
+		if(q.equalsIgnoreCase("network_count"))
+		{
+			return Phantom.getNetworkCount() + "";
+		}
+		
+		for(String i : Phantom.getServers())
+		{
+			if(q.equalsIgnoreCase("server_" + i.toLowerCase() + "_count"))
+			{
+				return Phantom.getNetworkCount(i) + "";
+			}
+		}
+		
+		if(q.equalsIgnoreCase("ping"))
+		{
+			return NMSX.ping(p) + "ms";
+		}
+		
+		return null;
+	}
+}
