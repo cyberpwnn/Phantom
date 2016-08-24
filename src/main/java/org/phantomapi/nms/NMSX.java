e4dba71c9cd31652022d1b82d4b0e23ccc0efe29
Daniel Mills <danielmillst@gmail.com>
2016-08-24 16:24:23 -0400

    Slate Abstraction

--- src/main/java/org/phantomapi/nms/NMSX.java
+++ src/main/java/org/phantomapi/nms/NMSX.java
@@ -37,6 +37,162 @@ public class NMSX
 	public static String nmsver;
 	public static GList<Color> colors;
 	
+	public static String getVersion()
+	{
+		final String name = Bukkit.getServer().getClass().getPackage().getName();
+		final String version = name.substring(name.lastIndexOf('.') + 1) + ".";
+		return version;
+	}
+	
+	public static Object serializeChat(String msg)
+	{
+		try
+		{
+			return NMSClass.ChatSerializer.getDeclaredMethod("a", String.class).invoke(null, msg);
+		}
+		
+		catch(Throwable e)
+		{
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Set a specified Field accessible
+	 *
+	 * @param f
+	 *            Field set accessible
+	 */
+	public static Field setAccessible(Field f) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
+	{
+		f.setAccessible(true);
+		Field modifiersField = Field.class.getDeclaredField("modifiers");
+		modifiersField.setAccessible(true);
+		modifiersField.setInt(f, f.getModifiers() & 0xFFFFFFEF);
+		
+		return f;
+	}
+	
+	/**
+	 * Set a specified Method accessible
+	 *
+	 * @param m
+	 *            Method set accessible
+	 */
+	public static Method setAccessible(Method m) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
+	{
+		m.setAccessible(true);
+		
+		return m;
+	}
+	
+	public static Class<?> getCBNMSClass(String className)
+	{
+		final String fullName = "net.minecraft.server." + getVersion() + className;
+		
+		Class<?> clazz = null;
+		
+		try
+		{
+			clazz = Class.forName(fullName);
+		}
+		
+		catch(final Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return clazz;
+	}
+	
+	public static Class<?> getOBCClass(String className)
+	{
+		final String fullName = "org.bukkit.craftbukkit." + getVersion() + className;
+		
+		Class<?> clazz = null;
+		
+		try
+		{
+			clazz = Class.forName(fullName);
+		}
+		
+		catch(final Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return clazz;
+	}
+	
+	public static Object getHandle(Object obj)
+	{
+		try
+		{
+			return getMethod(obj.getClass(), "getHandle", new Class[0]).invoke(obj, new Object[0]);
+		}
+		
+		catch(final Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public static Field getField(Class<?> clazz, String name)
+	{
+		try
+		{
+			final Field field = clazz.getDeclaredField(name);
+			field.setAccessible(true);
+			return field;
+		}
+		
+		catch(final Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public static Method getMethod(Class<?> clazz, String name, Class<?>... args)
+	{
+		for(final Method m : clazz.getMethods())
+		{
+			if(m.getName().equals(name) && (args.length == 0 || ClassListEqual(args, m.getParameterTypes())))
+			{
+				m.setAccessible(true);
+				return m;
+			}
+		}
+		
+		return null;
+	}
+	
+	public static boolean ClassListEqual(Class<?>[] l1, Class<?>[] l2)
+	{
+		boolean equal = true;
+		
+		if(l1.length != l2.length)
+		{
+			return false;
+		}
+		
+		for(int i = 0; i < l1.length; i++)
+		{
+			if(l1[i] != l2[i])
+			{
+				equal = false;
+				break;
+			}
+		}
+		
+		return equal;
+	}
+	
 	/**
 	 * Send a packet (get the handle and send it yada yada)
 	 * 
---
+++ src/main/java/org/phantomapi/nms/Tab.java
@@ -0,0 +1,120 @@
+package org.phantomapi.nms;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.bukkit.entity.Player;
+
+public class Tab
+{
+	protected static Object EMPTY;
+	
+	static
+	{
+		try
+		{
+			EMPTY = NMSX.getCBNMSClass("ChatComponentText").getConstructor(String.class).newInstance("");
+		}
+		
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	protected final Player player;
+	
+	public String[] header;
+	public String[] footer;
+	public List<TabItem> items = new ArrayList<TabItem>();
+	
+	public Tab(Player p)
+	{
+		this.player = p;
+	}
+	
+	public void clearItems()
+	{
+		updateItems(4);
+	}
+	
+	public void updateItems()
+	{
+		updateItems(0);
+	}
+	
+	@SuppressWarnings({"unchecked", "rawtypes"})
+	public void updateItems(int action)
+	{
+		try
+		{
+			List<Object> itemPackets = new ArrayList<Object>();
+			List<Object> packets = new ArrayList<Object>();
+
+			for(TabItem item : items)
+			{
+				itemPackets.add(item.toPacket(this, action));
+			}
+			
+			
+			if(NMSClass.version < 180)
+			{
+				packets.addAll(itemPackets);
+			}
+			
+			else
+			{
+				Object playerInfoPacket = NMSClass.PacketPlayOutPlayerInfo.newInstance();
+				NMSX.setAccessible(NMSClass.PacketPlayOutPlayerInfo.getDeclaredField("a")).set(playerInfoPacket, NMSClass.EnumPlayerInfoAction.getEnumConstants()[action]);
+				List<Object> dataList = (List) NMSX.setAccessible(NMSClass.PacketPlayOutPlayerInfo.getDeclaredField("b")).get(playerInfoPacket);
+				dataList.addAll(itemPackets);
+				packets.add(playerInfoPacket);
+			}
+			
+			for(Object packet : packets)
+			{
+				NMSX.sendPacket(player, packet);
+			}
+		}
+		
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateHeaderFooter()
+	{
+		if(header == null && footer == null)
+		{
+			return;
+		}
+		try
+		{
+			Object packet = NMSClass.PacketPlayOutPlayerListHeaderFooter.getConstructor(NMSClass.IChatBaseComponent).newInstance(new Object[] {null});
+			
+			Object serHeader = header == null ? EMPTY : NMSX.serializeChat(mergeJSON(header));
+			Object serFooter = footer == null ? EMPTY : NMSX.serializeChat(mergeJSON(footer));
+			
+			NMSX.setAccessible(NMSClass.PacketPlayOutPlayerListHeaderFooter.getDeclaredField("a")).set(packet, serHeader);
+			NMSX.setAccessible(NMSClass.PacketPlayOutPlayerListHeaderFooter.getDeclaredField("b")).set(packet, serFooter);
+			
+			NMSX.sendPacket(player, packet);
+		}
+		catch(Throwable e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private String mergeJSON(String... json)
+	{
+		String merged = "";
+		for(String s : json)
+		{
+			merged += s + ",";
+		}
+		merged = merged.substring(0, merged.length() - 1);
+		return String.format("{\"text\":\"\",\"extra\":[%s]}", merged);
+	}
+	
+}
