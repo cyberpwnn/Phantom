0561c0287bd6923c2388a8d35d364069883bad28
Daniel Mills <danielmillst@gmail.com>
2016-08-25 20:33:46 -0400

    Placeholder controller

---
+++ src/main/java/org/phantomapi/PlaceholderController.java
@@ -0,0 +1,57 @@
+package org.phantomapi;
+
+import java.util.UUID;
+import org.bukkit.entity.Player;
+import org.phantomapi.construct.Controllable;
+import org.phantomapi.construct.Controller;
+import org.phantomapi.lang.GMap;
+import org.phantomapi.placeholder.PlaceholderHook;
+
+public class PlaceholderController extends Controller
+{
+	private GMap<UUID, PlaceholderHook> hooks;
+	
+	public PlaceholderController(Controllable parentController)
+	{
+		super(parentController);
+		
+		hooks = new GMap<UUID, PlaceholderHook>();
+	}
+
+	@Override
+	public void onStart()
+	{
+		
+	}
+
+	@Override
+	public void onStop()
+	{
+		
+	}
+	
+	public void unhook(UUID uuid)
+	{
+		hooks.remove(uuid);
+	}
+	
+	public void hook(UUID uuid, PlaceholderHook hook)
+	{
+		hooks.put(uuid, hook);
+	}
+	
+	public String handle(Player p, String q)
+	{
+		for(UUID i : hooks.k())
+		{
+			String v = hooks.get(i).onPlaceholderRequest(p, q);
+			
+			if(v != null)
+			{
+				return v;
+			}
+		}
+		
+		return null;
+	}
+}
