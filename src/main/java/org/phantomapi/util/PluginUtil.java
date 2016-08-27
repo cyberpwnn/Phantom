fd7031087a6436264875dc35bf1c74f74f960c74
Daniel Mills <danielmillst@gmail.com>
2016-08-27 02:01:04 -0400

    Allow non gc unloading

--- src/main/java/org/phantomapi/util/PluginUtil.java
+++ src/main/java/org/phantomapi/util/PluginUtil.java
@@ -565,5 +565,131 @@ public class PluginUtil
 		
 		return;
 	}
+
+	@SuppressWarnings("unchecked")
+	public static void unloadNoGC(Plugin plugin)
+	{
+
+		String name = plugin.getName();
+		
+		PluginManager pluginManager = Bukkit.getPluginManager();
+		
+		SimpleCommandMap commandMap = null;
+		
+		List<Plugin> plugins = null;
+		
+		Map<String, Plugin> names = null;
+		Map<String, Command> commands = null;
+		Map<Event, SortedSet<RegisteredListener>> listeners = null;
+		
+		boolean reloadlisteners = true;
+		
+		if(pluginManager != null)
+		{
+			
+			pluginManager.disablePlugin(plugin);
+			
+			try
+			{
+				
+				Field pluginsField = Bukkit.getPluginManager().getClass().getDeclaredField("plugins");
+				pluginsField.setAccessible(true);
+				plugins = (List<Plugin>) pluginsField.get(pluginManager);
+				
+				Field lookupNamesField = Bukkit.getPluginManager().getClass().getDeclaredField("lookupNames");
+				lookupNamesField.setAccessible(true);
+				names = (Map<String, Plugin>) lookupNamesField.get(pluginManager);
+				
+				try
+				{
+					Field listenersField = Bukkit.getPluginManager().getClass().getDeclaredField("listeners");
+					listenersField.setAccessible(true);
+					listeners = (Map<Event, SortedSet<RegisteredListener>>) listenersField.get(pluginManager);
+				}
+				catch(Exception e)
+				{
+					reloadlisteners = false;
+				}
+				
+				Field commandMapField = Bukkit.getPluginManager().getClass().getDeclaredField("commandMap");
+				commandMapField.setAccessible(true);
+				commandMap = (SimpleCommandMap) commandMapField.get(pluginManager);
+				
+				Field knownCommandsField = SimpleCommandMap.class.getDeclaredField("knownCommands");
+				knownCommandsField.setAccessible(true);
+				commands = (Map<String, Command>) knownCommandsField.get(commandMap);
+				
+			}
+			catch(NoSuchFieldException e)
+			{
+				e.printStackTrace();
+				return;
+			}
+			catch(IllegalAccessException e)
+			{
+				e.printStackTrace();
+				return;
+			}
+		}
+		
+		pluginManager.disablePlugin(plugin);
+		
+		if(plugins != null && plugins.contains(plugin))
+			plugins.remove(plugin);
+		
+		if(names != null && names.containsKey(name))
+			names.remove(name);
+		
+		if(listeners != null && reloadlisteners)
+		{
+			for(SortedSet<RegisteredListener> set : listeners.values())
+			{
+				for(Iterator<RegisteredListener> it = set.iterator(); it.hasNext();)
+				{
+					RegisteredListener value = it.next();
+					if(value.getPlugin() == plugin)
+					{
+						it.remove();
+					}
+				}
+			}
+		}
+		
+		if(commandMap != null)
+		{
+			for(Iterator<Map.Entry<String, Command>> it = commands.entrySet().iterator(); it.hasNext();)
+			{
+				Map.Entry<String, Command> entry = it.next();
+				if(entry.getValue() instanceof PluginCommand)
+				{
+					PluginCommand c = (PluginCommand) entry.getValue();
+					if(c.getPlugin() == plugin)
+					{
+						c.unregister(commandMap);
+						it.remove();
+					}
+				}
+			}
+		}
+		
+		// Attempt to close the classloader to unlock any handles on the
+		// plugin's
+		// jar file.
+		ClassLoader cl = plugin.getClass().getClassLoader();
+		
+		if(cl instanceof URLClassLoader)
+		{
+			try
+			{
+				((URLClassLoader) cl).close();
+			}
+			catch(IOException ex)
+			{
+				Logger.getLogger(PluginUtil.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+		
+		return;
+	}
 	
 }
