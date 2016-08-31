dd09382f65e5ab8e57d0ef3502f67c1fd8ec6137
Daniel Mills <danielmillst@gmail.com>
2016-08-30 22:33:51 -0400

    A Networked server

---
+++ src/main/java/org/phantomapi/bungeecord/NetworkedServer.java
@@ -0,0 +1,40 @@
+package org.phantomapi.bungeecord;
+
+import org.phantomapi.lang.GList;
+
+/**
+ * Represents a remote networked server on a bungeecord network
+ * 
+ * @author cyberpwn
+ */
+public interface NetworkedServer
+{
+	/**
+	 * Get the bungeecord network
+	 * 
+	 * @return the network
+	 */
+	public Network getNetwork();
+	
+	/**
+	 * Get the name of this server
+	 * 
+	 * @return the name of the server
+	 */
+	public String getName();
+	
+	/**
+	 * Get the list of players on this remote server
+	 * 
+	 * @return the list of players
+	 */
+	public GList<String> getPlayers();
+	
+	/**
+	 * Check if this server is a remote server or is the remote server actually
+	 * this server
+	 * 
+	 * @return true if the server is not THIS server
+	 */
+	public Boolean isRemote();
+}
