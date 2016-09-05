446c519eccf8e656bcd21f40747db0ce3f9d4eaa
Daniel Mills <danielmillst@gmail.com>
2016-09-05 18:19:34 -0400

    ?

--- src/main/java/org/phantomapi/clust/YAMLDataInput.java
+++ src/main/java/org/phantomapi/clust/YAMLDataInput.java
@@ -2,7 +2,6 @@ package org.phantomapi.clust;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
