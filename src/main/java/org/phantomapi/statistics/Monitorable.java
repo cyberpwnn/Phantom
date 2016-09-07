fa708d8928c1a1669019e69e121d2a9912183fd0
Daniel Mills <danielmillst@gmail.com>
2016-09-07 16:23:33 -0400

    Monitorable

---
+++ src/main/java/org/phantomapi/statistics/Monitorable.java
@@ -0,0 +1,16 @@
+package org.phantomapi.statistics;
+
+/**
+ * Represents a monitorable object
+ * 
+ * @author cyberpwn
+ */
+public interface Monitorable
+{
+	/**
+	 * Gets the monitored data for this object
+	 * 
+	 * @return the monitored data
+	 */
+	public String getMonitorableData();
+}
