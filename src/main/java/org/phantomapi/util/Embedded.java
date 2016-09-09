7ee86593c19e1e89e40c943ad0ac52246a7ae3b1
Daniel Mills <danielmillst@gmail.com>
2016-09-09 02:02:57 -0400

    Hide

--- src/main/java/org/phantomapi/util/Embedded.java
+++ src/main/java/org/phantomapi/util/Embedded.java
@@ -13,6 +13,7 @@ import org.phantomapi.lang.GList;
  * @param <T>
  *            the interface to be embedded.
  */
+@SuppressWarnings("hiding")
 public interface Embedded<T>
 {
 	/**
--- src/main/java/org/phantomapi/util/RunVal.java
+++ src/main/java/org/phantomapi/util/RunVal.java
@@ -2,6 +2,7 @@ package org.phantomapi.util;
 
 import com.boydti.fawe.object.RunnableVal;
 
+@SuppressWarnings("hiding")
 public abstract class RunVal<T> extends RunnableVal<T>
 {
 	
