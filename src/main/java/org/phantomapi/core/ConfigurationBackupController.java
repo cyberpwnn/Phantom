27536baeae7ac2bbd973de1737baf3795de6580d
Daniel Mills <danielmillst@gmail.com>
2016-09-10 23:04:32 -0400

    Clean configs that are old

--- src/main/java/org/phantomapi/core/ConfigurationBackupController.java
+++ src/main/java/org/phantomapi/core/ConfigurationBackupController.java
@@ -3,28 +3,42 @@ package org.phantomapi.core;
 import java.io.File;
 import java.io.IOException;
 import org.phantomapi.async.A;
+import org.phantomapi.clust.Comment;
 import org.phantomapi.clust.Configurable;
+import org.phantomapi.clust.ConfigurableController;
 import org.phantomapi.clust.DataCluster;
+import org.phantomapi.clust.Keyed;
 import org.phantomapi.clust.YAMLDataOutput;
 import org.phantomapi.construct.Controllable;
 import org.phantomapi.construct.Controller;
 import org.phantomapi.construct.Ticked;
+import org.phantomapi.lang.GList;
 import org.phantomapi.lang.GMap;
+import org.phantomapi.lang.GTime;
 import org.phantomapi.sync.S;
 import org.phantomapi.sync.TaskLater;
 import org.phantomapi.util.F;
+import org.phantomapi.util.M;
 
 @Ticked(20)
-public class ConfigurationBackupController extends Controller
+public class ConfigurationBackupController extends ConfigurableController
 {
+	@Comment("Hours to pass before cleaning configs")
+	@Keyed("housekeeping.time-before-delete")
+	public double ttd = 72.0;
+	
 	private GMap<File, DataCluster> queue;
+	private GList<File> scannable;
 	private static boolean running;
+	private int k;
 	
 	public ConfigurationBackupController(Controllable parentController)
 	{
-		super(parentController);
+		super(parentController, "backup-manager");
 		
 		this.queue = new GMap<File, DataCluster>();
+		this.scannable = new GList<File>();
+		this.k = 60;
 		
 		running = false;
 	}
@@ -32,7 +46,7 @@ public class ConfigurationBackupController extends Controller
 	@Override
 	public void onStart()
 	{
-		
+		loadCluster(this);
 	}
 	
 	@Override
@@ -43,6 +57,8 @@ public class ConfigurationBackupController extends Controller
 	
 	public void onTick()
 	{
+		k++;
+		
 		new TaskLater(2)
 		{
 			@Override
@@ -73,7 +89,9 @@ public class ConfigurationBackupController extends Controller
 								
 								try
 								{
-									new YAMLDataOutput().save(asyncQueue.get(i), i);
+									File f = new File(i, "" + M.ms() + ".yml");
+									scannable.add(i);
+									new YAMLDataOutput().save(asyncQueue.get(i), f);
 									processed++;
 								}
 								
@@ -87,6 +105,49 @@ public class ConfigurationBackupController extends Controller
 							int f = failed;
 							
 							v("Backed up " + F.f(p) + " file(s) with " + F.f(f) + " failures.");
+							
+							new S()
+							{
+								@Override
+								public void sync()
+								{
+									running = false;
+								}
+							};
+						}
+					};
+				}
+				
+				if(!running && queue.isEmpty() && !scannable.isEmpty() && k > 60)
+				{
+					k = 0;
+					running = true;
+					
+					new A()
+					{
+						@Override
+						public void async()
+						{
+							int del = 0;
+							
+							for(File i : scannable)
+							{
+								for(File j : i.listFiles())
+								{
+									if(new GTime(M.ms() - j.lastModified()).getHours() > ttd && i.listFiles().length > 1)
+									{
+										if(j.delete())
+										{
+											del++;
+										}
+									}
+								}
+							}
+							
+							if(del > 0)
+							{
+								w("Cleaned " + del + " old backups.");
+							}
 							
 							new S()
 							{
