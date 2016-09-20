bbd2f3b41ae89f00f73c6141f492f6ee92ecbc7a
Daniel Mills <danielmillst@gmail.com>
2016-09-19 20:37:22 -0400

    Wraith despawn reason

---
+++ src/main/java/org/phantomapi/npc/WraithDespawn.java
@@ -0,0 +1,39 @@
+package org.phantomapi.npc;
+
+/**
+ * Despawn reason for wraith npcs
+ * 
+ * @author cyberpwn
+ */
+public enum WraithDespawn
+{
+	/**
+	 * Wraith despawned because of a chunk unload
+	 */
+	CHUNK_UNLOAD,
+	
+	/**
+	 * Wraith despawned because of death
+	 */
+	DEATH,
+	
+	/**
+	 * Wraith despawned because of an upcoming respawn
+	 */
+	PENDING_RESPAWN,
+	
+	/**
+	 * Wraith despawned because of a plugin
+	 */
+	PLUGIN,
+	
+	/**
+	 * Wraith despawned because it was removed
+	 */
+	REMOVAL,
+	
+	/**
+	 * Wraith despawned because the world unloaded
+	 */
+	WORLD_UNLOAD;
+}
