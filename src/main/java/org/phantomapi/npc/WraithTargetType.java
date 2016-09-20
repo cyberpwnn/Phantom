f9787237867b950390e80cb653a616edff07f984
Daniel Mills <danielmillst@gmail.com>
2016-09-19 21:07:59 -0400

    Wraith target type

---
+++ src/main/java/org/phantomapi/npc/WraithTargetType.java
@@ -0,0 +1,24 @@
+package org.phantomapi.npc;
+
+/**
+ * Represents a target type
+ * 
+ * @author cyberpwn
+ */
+public enum WraithTargetType
+{
+	/**
+	 * The target is an entity
+	 */
+	ENTITY,
+	
+	/**
+	 * The target is a location
+	 */
+	LOCATION,
+	
+	/**
+	 * No target
+	 */
+	NONE
+}
