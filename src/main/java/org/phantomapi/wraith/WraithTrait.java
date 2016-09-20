4f7f40e2f31e9b5c8bdfa03cecd89f2bdfd5eb7e
Daniel Mills <danielmillst@gmail.com>
2016-09-19 21:33:47 -0400

    Better wraith trait

--- src/main/java/org/phantomapi/wraith/WraithTrait.java
+++ src/main/java/org/phantomapi/wraith/WraithTrait.java
@@ -1,6 +1,23 @@
 package org.phantomapi.wraith;
 
-public interface WraithTrait
+import net.citizensnpcs.api.trait.Trait;
+
+public abstract class WraithTrait extends Trait
 {
-	
+	protected WraithTrait(String name)
+	{
+		super(name);
+	}
+
+	@Override
+	public abstract void onAttach();
+
+	@Override
+	public abstract void onDespawn();
+
+	@Override
+	public abstract void onSpawn();
+
+	@Override
+	public abstract void run();
 }
