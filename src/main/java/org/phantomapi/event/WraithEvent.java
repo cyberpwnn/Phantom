ba428a707dcf517486f2446518f0f6167ae6cb7a
Daniel Mills <danielmillst@gmail.com>
2016-09-21 07:00:31 -0400

    Base wraith event

--- src/main/java/org/phantomapi/event/WraithEvent.java
+++ src/main/java/org/phantomapi/event/WraithEvent.java
@@ -1,6 +1,28 @@
 package org.phantomapi.event;
 
+import org.phantomapi.wraith.Wraith;
+
+/**
+ * Represents a wraith event
+ * 
+ * @author cyberpwn
+ */
 public class WraithEvent extends CancellablePhantomEvent
 {
+	private final Wraith wraith;
 	
+	public WraithEvent(Wraith wraith)
+	{
+		this.wraith = wraith;
+	}
+	
+	/**
+	 * Get the wraith associated with this event
+	 * 
+	 * @return the wraith
+	 */
+	public Wraith getWraith()
+	{
+		return wraith;
+	}
 }
