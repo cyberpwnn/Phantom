edb96036dd58bb9ba7d8960100b7440f8f942e7d
Daniel Mills <danielmillst@gmail.com>
2016-09-21 06:35:32 -0400

    Wraith impl

---
+++ src/main/java/org/phantomapi/wraith/PhantomWraith.java
@@ -0,0 +1,35 @@
+package org.phantomapi.wraith;
+
+import org.phantomapi.lang.GList;
+
+public class PhantomWraith extends PhantomNPCWrapper implements Wraith
+{
+	private GList<WraithHandler> handlers;
+	
+	public PhantomWraith(String name, String skin)
+	{
+		super(name, skin);
+		
+		handlers = new GList<WraithHandler>();
+	}
+	
+	public PhantomWraith(String name)
+	{
+		this(name, name);
+	}
+	
+	public GList<WraithHandler> getHandlers()
+	{
+		return handlers;
+	}
+	
+	public void registerHandler(WraithHandler handler)
+	{
+		handlers.add(handler);
+	}
+	
+	public void unregisterHandler(WraithHandler handler)
+	{
+		handlers.remove(handler);
+	}
+}
