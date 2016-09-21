b5ec9f36501875e125ec52c174eb628429bd0559
Daniel Mills <danielmillst@gmail.com>
2016-09-21 06:51:00 -0400

    Wraith controller

---
+++ src/main/java/org/phantomapi/core/WraithController.java
@@ -0,0 +1,46 @@
+package org.phantomapi.core;
+
+import org.phantomapi.construct.Controllable;
+import org.phantomapi.construct.Controller;
+import org.phantomapi.lang.GList;
+import org.phantomapi.wraith.Wraith;
+
+public class WraithController extends Controller
+{
+	private GList<Wraith> wraiths;
+	
+	public WraithController(Controllable parentController)
+	{
+		super(parentController);
+		
+		wraiths = new GList<Wraith>();
+	}
+
+	@Override
+	public void onStart()
+	{
+		
+	}
+
+	@Override
+	public void onStop()
+	{
+		for(Wraith i : wraiths.copy())
+		{
+			i.despawn();
+			unRegisterWraith(i);
+		}
+		
+		wraiths.clear();
+	}
+	
+	public void registerWraith(Wraith wraith)
+	{
+		wraiths.add(wraith);
+	}
+	
+	public void unRegisterWraith(Wraith wraith)
+	{
+		wraiths.remove(wraith);
+	}
+}
