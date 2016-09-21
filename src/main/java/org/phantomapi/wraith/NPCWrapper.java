7b51b7e0c9f926ff16d8301b9fad55df08ee1eca
Daniel Mills <danielmillst@gmail.com>
2016-09-21 09:21:56 -0400

    Remove unsupported

--- src/main/java/org/phantomapi/wraith/NPCWrapper.java
+++ src/main/java/org/phantomapi/wraith/NPCWrapper.java
@@ -2,7 +2,6 @@ package org.phantomapi.wraith;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.ItemStack;
 
 public interface NPCWrapper
@@ -17,11 +16,7 @@ public interface NPCWrapper
 	
 	public void teleport(Location location);
 	
-	public Entity getTarget();
-	
-	public void setTarget(Entity entity);
-	
-	public LivingEntity getEntity();
+	public Entity getEntity();
 	
 	public int getEntityId();
 	
@@ -29,47 +24,11 @@ public interface NPCWrapper
 	
 	public void look(Location location);
 	
-	public void setPassenger(Entity entity);
-	
-	public Entity getPassenger();
-	
-	public void setYawPitch(float yaw, float pitch);
-	
-	public void setYaw(float yaw);
-	
-	public Float getYaw();
-	
-	public void setPitch(float pitch);
-	
-	public Float getPitch();
-	
 	public void setEquipment(WraithEquipment slot, ItemStack item);
 	
 	public ItemStack getEquipment(WraithEquipment slot);
 	
-	public void setInvulnerable(boolean invulnerable);
-	
-	public Boolean isInvulnerable();
-	
-	public void setCollision(boolean collision);
-	
-	public Boolean hasCollision();
-	
-	public void setControllable(boolean controllable);
-	
-	public Boolean isControllable();
-	
-	public void setFrozen(boolean frozen);
-	
-	public Boolean isFrozen();
-	
 	public String getName();
 	
 	public void setName(String name);
-	
-	public String getSkin();
-	
-	public void setSkin(String skin);
-	
-	public void reset();
 }
