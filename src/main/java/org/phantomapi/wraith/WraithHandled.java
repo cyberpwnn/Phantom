678716e124b86d6306bdd52f4021161b18879f38
Daniel Mills <danielmillst@gmail.com>
2016-09-21 06:41:21 -0400

    Wraith handled

---
+++ src/main/java/org/phantomapi/wraith/WraithHandled.java
@@ -0,0 +1,12 @@
+package org.phantomapi.wraith;
+
+import org.phantomapi.lang.GList;
+
+public interface WraithHandled
+{
+	public GList<WraithHandler> getHandlers();
+	
+	public void registerHandler(WraithHandler handler);
+	
+	public void unregisterHandler(WraithHandler handler);
+}
