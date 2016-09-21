29f620ffbc8089e51109a1b5f0cede6e8e01aa77
Daniel Mills <danielmillst@gmail.com>
2016-09-21 12:48:01 -0400

    Wraith handler jdoc

--- src/main/java/org/phantomapi/wraith/WraithHandler.java
+++ src/main/java/org/phantomapi/wraith/WraithHandler.java
@@ -2,11 +2,29 @@ package org.phantomapi.wraith;
 
 import org.bukkit.event.Listener;
 
+/**
+ * Represents a wraith handler
+ * 
+ * @author cyberpwn
+ */
 public interface WraithHandler extends Listener
 {
+	/**
+	 * Get the wraith
+	 * 
+	 * @return the wraith
+	 */
 	public Wraith getWraith();
 	
+	/**
+	 * Get the name of this handler
+	 * 
+	 * @return the handler name
+	 */
 	public String getName();
 	
+	/**
+	 * Unregister this wraith handler
+	 */
 	public void unregister();
 }
