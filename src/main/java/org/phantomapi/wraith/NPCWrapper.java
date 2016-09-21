204c9e2ee20943d0d0b14e22c43ec8a4a80b47a8
Daniel Mills <danielmillst@gmail.com>
2016-09-21 10:08:02 -0400

    Targeting and chatting

--- src/main/java/org/phantomapi/wraith/NPCWrapper.java
+++ src/main/java/org/phantomapi/wraith/NPCWrapper.java
@@ -2,7 +2,9 @@ package org.phantomapi.wraith;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.phantomapi.lang.GList;
 
 public interface NPCWrapper
 {
@@ -20,8 +22,14 @@ public interface NPCWrapper
 	
 	public int getEntityId();
 	
-	public void navigateTo(Location location);
+	public void target(Location location);
 	
+	public void target(Entity entity);
+	
+	public WraithTarget getTarget();
+	
+	public void updateTarget();
+		
 	public void look(Location location);
 	
 	public void setEquipment(WraithEquipment slot, ItemStack item);
@@ -31,4 +39,44 @@ public interface NPCWrapper
 	public String getName();
 	
 	public void setName(String name);
+	
+	public void setSneaking(boolean sneaking);
+	
+	public boolean isSneaking();
+	
+	public void setSprinting(boolean sprinting);
+	
+	public boolean isSprinting();
+	
+	public boolean isAllowedFlight();
+	
+	public void allowFlight(boolean flightFinding);
+	
+	public Player getPlayer();
+	
+	public void setProtected(boolean protect);
+	
+	public boolean isProtected();
+	
+	public boolean hasTarget();
+	
+	public void clearTarget();
+	
+	public void say(String message);
+	
+	public void say(String message, double radius);
+	
+	public void say(String message, Player p);
+	
+	public void say(String message, Player... players);
+	
+	public void say(String message, GList<Player> players);
+	
+	public String getChatName();
+	
+	public String getChatHover();
+	
+	public void setChatName(String name);
+	
+	public void setChatHover(String hover);
 }
