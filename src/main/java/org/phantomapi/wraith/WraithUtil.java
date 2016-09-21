d691fe64a7ce7b0270d989f999619c24c42139a1
Daniel Mills <danielmillst@gmail.com>
2016-09-21 10:55:31 -0400

    Wraith utils

--- src/main/java/org/phantomapi/wraith/WraithUtil.java
+++ src/main/java/org/phantomapi/wraith/WraithUtil.java
@@ -1,8 +1,10 @@
 package org.phantomapi.wraith;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.phantomapi.Phantom;
 import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.npc.NPC;
 
 /**
  * Wraith utils
@@ -33,5 +35,35 @@ public class WraithUtil
 	public static Wraith getWraith(Player p)
 	{
 		return Phantom.instance().getWraithController().get(p.getEntityId());
+	}
+	
+	/**
+	 * Get the wraith from this npc
+	 * 
+	 * @param npc
+	 *            the npc
+	 * @return the wraith
+	 */
+	public static Wraith getWraith(NPC npc)
+	{
+		if(isWraith(npc))
+		{
+			return Phantom.instance().getWraithController().get(npc.getEntity().getEntityId());
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Is the given npc a player, wraith and registered within the wraith
+	 * controller?
+	 * 
+	 * @param npc
+	 *            the npc
+	 * @return true if it is a wraith
+	 */
+	public static boolean isWraith(NPC npc)
+	{
+		return npc.getEntity().getType().equals(EntityType.PLAYER) && isWraith((Player) npc.getEntity());
 	}
 }
