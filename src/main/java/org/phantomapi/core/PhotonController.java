22f1b01c294041a28ec75d12402b9bdf11a8c2ed
Daniel Mills <danielmillst@gmail.com>
2016-09-22 03:57:03 -0400

    Photon controller

---
+++ src/main/java/org/phantomapi/core/PhotonController.java
@@ -0,0 +1,74 @@
+package org.phantomapi.core;
+
+import org.bukkit.Chunk;
+import org.phantomapi.construct.Controllable;
+import org.phantomapi.construct.Controller;
+import org.phantomapi.construct.Ticked;
+import org.phantomapi.lang.GList;
+import org.phantomapi.lang.GSet;
+import org.phantomapi.statistics.Monitorable;
+import org.phantomapi.util.C;
+import org.phantomapi.world.L;
+
+/**
+ * Relighting
+ * 
+ * @author cyberpwn
+ */
+@Ticked(20)
+public class PhotonController extends Controller implements Monitorable
+{
+	private GSet<Chunk> queue;
+	private Boolean wait;
+	
+	public PhotonController(Controllable parentController)
+	{
+		super(parentController);
+		
+		queue = new GSet<Chunk>();
+		wait = false;
+	}
+	
+	@Override
+	public void onStart()
+	{
+		
+	}
+	
+	@Override
+	public void onStop()
+	{
+		
+	}
+	
+	public void onTick()
+	{
+		if(wait)
+		{
+			wait = false;
+			return;
+		}
+		
+		if(!queue.isEmpty())
+		{
+			for(Chunk i : new GList<Chunk>(queue))
+			{
+				L.relight(i);
+			}
+			
+			queue.clear();
+		}
+	}
+	
+	public void relight(Chunk chunk)
+	{
+		wait = true;
+		queue.add(chunk);
+	}
+	
+	@Override
+	public String getMonitorableData()
+	{
+		return "Photons: " + C.LIGHT_PURPLE + queue.size() + " " + (wait ? C.RED + "> WAITING" : (queue.isEmpty() ? "" : C.AQUA + "> RUNNING"));
+	}
+}
