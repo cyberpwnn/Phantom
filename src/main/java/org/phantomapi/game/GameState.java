ba6f220540b7486af9554de2bb035ad604fe3a5f
Daniel Mills <danielmillst@gmail.com>
2016-09-23 12:46:36 -0400

    Game States

---
+++ src/main/java/org/phantomapi/game/GameState.java
@@ -0,0 +1,16 @@
+package org.phantomapi.game;
+
+/**
+ * Represents a game state
+ * 
+ * @author cyberpwn
+ */
+public interface GameState
+{
+	/**
+	 * Get the game
+	 * 
+	 * @return the game instance
+	 */
+	public Game getGame();
+}
