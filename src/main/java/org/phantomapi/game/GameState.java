b8e32f75dd1a705ca7dfa7a8a7f7e9f61d2cda21
Daniel Mills <danielmillst@gmail.com>
2016-09-25 10:07:35 -0400

    Game state objects

--- src/main/java/org/phantomapi/game/GameState.java
+++ src/main/java/org/phantomapi/game/GameState.java
@@ -1,5 +1,7 @@
 package org.phantomapi.game;
 
+import org.phantomapi.lang.GMap;
+
 /**
  * Represents a game state
  * 
@@ -13,4 +15,20 @@ public interface GameState
 	 * @return the game instance
 	 */
 	public Game getGame();
+	
+	/**
+	 * Get all game objects
+	 * 
+	 * @return the game objects
+	 */
+	public GMap<String, GMap<String, GameObject>> getObjects();
+	
+	/**
+	 * Get all game objects from the given type
+	 * 
+	 * @param type
+	 *            the type
+	 * @return the game objects or null
+	 */
+	public GMap<String, GameObject> getObjects(String type);
 }
