83b775f97e6cbba8d870d3cb9e72b93ef7b9a564
Daniel Mills <danielmillst@gmail.com>
2016-09-25 10:07:41 -0400

    Game state impl

--- src/main/java/org/phantomapi/game/PhantomGameState.java
+++ src/main/java/org/phantomapi/game/PhantomGameState.java
@@ -1,5 +1,7 @@
 package org.phantomapi.game;
 
+import org.phantomapi.lang.GMap;
+
 /**
  * Represents a game state
  * 
@@ -8,6 +10,8 @@ package org.phantomapi.game;
 public class PhantomGameState implements GameState
 {
 	protected final Game game;
+	private final GMap<String, GMap<String, GameObject>> objects;
+	private final GMap<String, GMap<Object, String>> reference;
 	
 	/**
 	 * Create a game state
@@ -18,10 +22,37 @@ public class PhantomGameState implements GameState
 	public PhantomGameState(Game game)
 	{
 		this.game = game;
+		this.objects = new GMap<String, GMap<String, GameObject>>();
+		this.reference = new GMap<String, GMap<Object, String>>();
 	}
 	
 	public Game getGame()
 	{
 		return game;
+	}
+
+	public GMap<String, GMap<String, GameObject>> getObjects()
+	{
+		return objects;
+	}
+	
+	public GMap<String, GameObject> getObjects(String type)
+	{
+		return objects.get(type);
+	}
+	
+	public GameObject get(String type, Object reference)
+	{
+		return getObjects(type).get(this.reference.get(type).get(reference));
+	}
+	
+	public void reference(String type, GameObject object, String reference)
+	{
+		if(!this.reference.containsKey(type))
+		{
+			this.reference.put(type, new GMap<Object, String>());
+		}
+		
+		this.reference.get(type).put(object, reference);
 	}
 }
