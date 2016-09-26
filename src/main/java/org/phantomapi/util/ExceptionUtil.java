6366de71454efb15e7103eed755dd8020e624044
Daniel Mills <danielmillst@gmail.com>
2016-09-26 12:50:15 -0400

    Update Exception utils

--- src/main/java/org/phantomapi/util/ExceptionUtil.java
+++ src/main/java/org/phantomapi/util/ExceptionUtil.java
@@ -1,5 +1,7 @@
 package org.phantomapi.util;
 
+import org.phantomapi.lang.GList;
+
 /**
  * Exception utils
  * 
@@ -13,15 +15,49 @@ public class ExceptionUtil
 	 * @param e
 	 *            the exception
 	 */
-	public static void print(Exception e)
+	public static void print(Throwable e)
 	{
 		D d = new D("EX");
-		d.f(e.getClass().getSimpleName() + " thrown at " + C.YELLOW + e.getStackTrace()[0].getClassName());
+		d.f(e.getClass().getSimpleName() + " thrown at " + C.YELLOW + e.getStackTrace()[0].getClassName() + C.LIGHT_PURPLE + "." + e.getStackTrace()[0].getMethodName() + "(" + e.getStackTrace()[0].getLineNumber() + ")");
 		d.w(e.getMessage());
+		int k = 0;
 		
-		for(StackTraceElement i : e.getStackTrace())
+		for(StackTraceElement i : new GList<StackTraceElement>(e.getStackTrace()))
 		{
-			d.i("  " + i.getClassName() + "." + C.YELLOW + i.getMethodName() + "(" + i.getLineNumber() + ")");
+			String s = F.repeat(" ", k) + "  \\" + i.getClassName() + "." + C.YELLOW + i.getMethodName() + C.LIGHT_PURPLE + "(" + i.getLineNumber() + ")";
+			
+			if(i.getClassName().equals(e.getStackTrace()[0].getClassName()))
+			{
+				s = C.RED + s;
+			}
+			
+			else if(i.getClassName().startsWith("net.minecraft.server."))
+			{
+				s = C.DARK_BLUE + s;
+			}
+			
+			else if(i.getClassName().startsWith("org.bukkit.craftbukkit"))
+			{
+				s = C.DARK_GREEN + s;
+			}
+			
+			else if(i.getClassName().startsWith("org.bukkit."))
+			{
+				s = C.DARK_AQUA + s;
+			}
+			
+			else if(i.getClassName().startsWith("org.phantomapi."))
+			{
+				s = C.DARK_PURPLE + s;
+			}
+			
+			else
+			{
+				s = C.DARK_GRAY + s;
+			}
+			
+			d.f(s);
+			k++;
 		}
 	}
 }
