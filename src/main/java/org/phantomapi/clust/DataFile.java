824c27e05dca3f58fd9e6be917bef7ce9222ea94
Daniel Mills <danielmillst@gmail.com>
2016-09-26 13:53:36 -0400

    Data files

---
+++ src/main/java/org/phantomapi/clust/DataFile.java
@@ -0,0 +1,68 @@
+package org.phantomapi.clust;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ * Represents a data file
+ * 
+ * @author cyberpwn
+ */
+public class DataFile extends DataCluster implements DataEntity
+{
+	/**
+	 * Load data from the given file. If it doesnt exist it will be created
+	 * 
+	 * @param file
+	 *            the file
+	 * @throws IOException
+	 *             shit happens
+	 */
+	public void load(File file) throws IOException
+	{
+		if(!file.exists())
+		{
+			file.getParentFile().mkdirs();
+			file.createNewFile();
+			return;
+		}
+		
+		clear();
+		fromData(Files.readAllBytes(file.toPath()));
+	}
+	
+	/**
+	 * Save the file. If it does not exist it will be created
+	 * 
+	 * @param file
+	 *            the file
+	 * @throws IOException
+	 *             this happens
+	 */
+	public void save(File file) throws IOException
+	{
+		if(!file.exists())
+		{
+			file.getParentFile().mkdirs();
+			file.createNewFile();
+		}
+		
+		FileOutputStream fos = new FileOutputStream(file, false);
+		fos.write(toData());
+		fos.close();
+	}
+	
+	@Override
+	public byte[] toData() throws IOException
+	{
+		return compress();
+	}
+	
+	@Override
+	public void fromData(byte[] data) throws IOException
+	{
+		addCompressed(data);
+	}
+}
