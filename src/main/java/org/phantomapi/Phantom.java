4f74eaf819b458c7fc2e86da01aa3ee166fb5464
Daniel Mills <danielmillst@gmail.com>
2016-09-27 00:16:26 -0400

    NestController closes #31

--- src/main/java/org/phantomapi/Phantom.java
+++ src/main/java/org/phantomapi/Phantom.java
@@ -35,6 +35,7 @@ import org.phantomapi.core.Metrics;
 import org.phantomapi.core.Metrics.Graph;
 import org.phantomapi.core.MonitorController;
 import org.phantomapi.core.MySQLConnectionController;
+import org.phantomapi.core.NestController;
 import org.phantomapi.core.NotificationController;
 import org.phantomapi.core.PhotonController;
 import org.phantomapi.core.PlaceholderController;
@@ -116,6 +117,7 @@ public class Phantom extends PhantomPlugin implements TagProvider
 	private PhotonController photonController;
 	private SpeechMesh saltpile;
 	private ResourceController resourceController;
+	private NestController nestController;
 	
 	private Long nsx;
 	
@@ -159,6 +161,7 @@ public class Phantom extends PhantomPlugin implements TagProvider
 		wraithController = new WraithController(this);
 		photonController = new PhotonController(this);
 		resourceController = new ResourceController(this);
+		nestController = new NestController(this);
 		bindings = new GList<Controllable>();
 		msgx = new GList<String>();
 		thread = Thread.currentThread().getId();
@@ -183,6 +186,7 @@ public class Phantom extends PhantomPlugin implements TagProvider
 		register(editSessionController);
 		register(wraithController);
 		register(photonController);
+		register(nestController);
 		
 		envFile = new File(getDataFolder().getParentFile().getParentFile(), "phantom-environment.json");
 		globalRegistry = new GlobalRegistry();
@@ -1621,6 +1625,11 @@ public class Phantom extends PhantomPlugin implements TagProvider
 		return resourceController;
 	}
 	
+	public NestController getNestController()
+	{
+		return nestController;
+	}
+
 	private void buildSaltpile()
 	{
 		GList<String> msg = new GList<String>();
---
+++ src/main/java/org/phantomapi/core/NestController.java
@@ -0,0 +1,72 @@
+package org.phantomapi.core;
+
+import java.io.IOException;
+import org.bukkit.Chunk;
+import org.bukkit.block.Block;
+import org.phantomapi.construct.Controllable;
+import org.phantomapi.construct.Controller;
+import org.phantomapi.lang.GMap;
+import org.phantomapi.nest.NestedBlock;
+import org.phantomapi.nest.NestedChunk;
+import org.phantomapi.nest.PhantomChunkNest;
+import org.phantomapi.util.ExceptionUtil;
+
+public class NestController extends Controller
+{
+	private GMap<Chunk, PhantomChunkNest> nests;
+	
+	public NestController(Controllable parentController)
+	{
+		super(parentController);
+		
+		nests = new GMap<Chunk, PhantomChunkNest>();
+	}
+
+	@Override
+	public void onStart()
+	{
+		
+	}
+
+	@Override
+	public void onStop()
+	{
+		
+	}
+	
+	public NestedChunk get(Chunk c)
+	{
+		if(!c.isLoaded())
+		{
+			return null;
+		}
+		
+		if(!nests.contains(c))
+		{
+			nests.put(c, new PhantomChunkNest(c));
+			
+			try
+			{
+				nests.get(c).load();
+			}
+			
+			catch(IOException e)
+			{
+				f("Failed to load nest");
+				ExceptionUtil.print(e);
+			}
+		}
+		
+		return nests.get(c);
+	}
+	
+	public NestedBlock get(Block block)
+	{
+		if(!block.getChunk().isLoaded())
+		{
+			return null;
+		}
+		
+		return get(block.getChunk()).getBlock(block);
+	}
+}
