32fcbb8c88442e0e90145bb0dc13de99b5a7b2c2
Daniel Mills <danielmillst@gmail.com>
2016-09-26 23:56:35 -0400

    Nest util jdoc

--- src/main/java/org/phantomapi/nest/NestUtil.java
+++ src/main/java/org/phantomapi/nest/NestUtil.java
@@ -3,8 +3,20 @@ package org.phantomapi.nest;
 import java.io.File;
 import org.bukkit.Chunk;
 
+/**
+ * Nest Utilities
+ * 
+ * @author cyberpwn
+ */
 public class NestUtil
 {
+	/**
+	 * Get the file name for a chunk nest
+	 * 
+	 * @param chunk
+	 *            the chunk
+	 * @return the file representation
+	 */
 	public static File getFile(Chunk chunk)
 	{
 		return new File(new File(chunk.getWorld().getWorldFolder(), "nest"), "n." + chunk.getX() + "." + chunk.getZ() + ".nst");
