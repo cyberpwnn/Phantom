5c52e2cfb5e7c988a8f3862e0591d9ecf4cb6a72
Daniel Mills <danielmillst@gmail.com>
2016-09-26 23:57:04 -0400

    PhantomBlockNest javadoc

--- src/main/java/org/phantomapi/nest/PhantomBlockNest.java
+++ src/main/java/org/phantomapi/nest/PhantomBlockNest.java
@@ -3,11 +3,24 @@ package org.phantomapi.nest;
 import org.bukkit.block.Block;
 import org.phantomapi.clust.DataCluster;
 
+/**
+ * Implementation of NestedBlock
+ * 
+ * @author cyberpwn
+ */
 public class PhantomBlockNest implements NestedBlock
 {
 	private DataCluster cc;
 	private Block block;
 	
+	/**
+	 * Create a block nest
+	 * 
+	 * @param block
+	 *            the block
+	 * @param cc
+	 *            the data cluster
+	 */
 	public PhantomBlockNest(Block block, DataCluster cc)
 	{
 		this.block = block;
@@ -18,7 +31,7 @@ public class PhantomBlockNest implements NestedBlock
 	{
 		return cc;
 	}
-
+	
 	public Block getBlock()
 	{
 		return block;
