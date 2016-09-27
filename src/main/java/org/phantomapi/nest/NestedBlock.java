6ae890f906885b070250bac0acdd78132bd9bd4c
Daniel Mills <danielmillst@gmail.com>
2016-09-26 23:56:51 -0400

    NestedBlock javadoc

--- src/main/java/org/phantomapi/nest/NestedBlock.java
+++ src/main/java/org/phantomapi/nest/NestedBlock.java
@@ -3,9 +3,25 @@ package org.phantomapi.nest;
 import org.bukkit.block.Block;
 import org.phantomapi.clust.DataCluster;
 
+/**
+ * Represents a nested block
+ * 
+ * @author cyberpwn
+ */
 public interface NestedBlock
 {
+	/**
+	 * Get the data cluster associated with this block. Direct modifications are
+	 * fine. This is not a cloned cluster
+	 * 
+	 * @return the un-cloned data cluster for this block
+	 */
 	public DataCluster getData();
-
+	
+	/**
+	 * Get the block for this nest
+	 * 
+	 * @return the block
+	 */
 	public Block getBlock();
 }
