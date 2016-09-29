52f41296e48223c7579602cfac2dd128ca944d87
Daniel Mills <danielmillst@gmail.com>
2016-09-29 04:41:29 -0400

    Capacity Modes

---
+++ src/main/java/org/phantomapi/game/CapacityMode.java
@@ -0,0 +1,19 @@
+package org.phantomapi.game;
+
+/**
+ * Game Capacity mode
+ * 
+ * @author cyberpwn
+ */
+public enum CapacityMode
+{
+	/**
+	 * Unlimited players
+	 */
+	UNLIMITED,
+	
+	/**
+	 * Limited players
+	 */
+	LIMITED;
+}
