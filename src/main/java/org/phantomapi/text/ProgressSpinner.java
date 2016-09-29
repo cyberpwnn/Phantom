4a6518c44c47138da94766e648ae60b83009f464
Daniel Mills <danielmillst@gmail.com>
2016-09-29 04:18:21 -0400

    Progress spinner

--- src/main/java/org/phantomapi/text/ProgressSpinner.java
+++ src/main/java/org/phantomapi/text/ProgressSpinner.java
@@ -10,6 +10,11 @@ public class ProgressSpinner
 {
 	private String[] chars;
 	private int index;
+	public static final ProgressSpinner DEFAULT;
+	public static final ProgressSpinner NETWORK;
+	public static final ProgressSpinner CIRCLES;
+	public static final ProgressSpinner RANDOMS;
+	public static final ProgressSpinner MERGERS;
 	
 	/**
 	 * Create a custom spinner
@@ -28,7 +33,7 @@ public class ProgressSpinner
 	 */
 	public ProgressSpinner()
 	{
-		this("" + '\u25d0', "" + '\u25d3', "" + '\u25d4', "" + '\u25d1', "" + '\u25d5');
+		this("" + '\u25d0', "" + '\u25d3', "" + '\u25d4', "" + '\u25d1', "" + '\u25d5', "" + '\u25d2');
 	}
 	
 	/**
@@ -47,5 +52,26 @@ public class ProgressSpinner
 		}
 		
 		return chars[index] + "";
+	}
+	
+	public String[] getChars()
+	{
+		return chars;
+	}
+
+	public int getIndex()
+	{
+		return index;
+	}
+
+
+
+	static
+	{
+		DEFAULT = new ProgressSpinner();
+		NETWORK = new ProgressSpinner("\u2630", "\u2631", "\u2632", "\u2633", "\u2634", "\u2635", "\u2636", "\u2637");
+		CIRCLES = new ProgressSpinner("\u25F4", "\u25F5", "\u25F6", "\u25F7");
+		RANDOMS = new ProgressSpinner("\u2680", "\u2681", "\u2682", "\u2683", "\u2684", "\u2685");
+		MERGERS = new ProgressSpinner("\u26AC", "\u26AD", "\u26AE", "\u26AF", "\u26AE", "\u26AD");
 	}
 }
