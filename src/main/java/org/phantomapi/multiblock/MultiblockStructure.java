adc4813ebaa5ec4f1871f7cbbbbfb5ce3e143090
Daniel Mills <danielmillst@gmail.com>
2016-10-02 02:12:42 -0400

    Base multiblock structure

---
+++ src/main/java/org/phantomapi/multiblock/MultiblockStructure.java
@@ -0,0 +1,16 @@
+package org.phantomapi.multiblock;
+
+public class MultiblockStructure
+{
+	private final String type;
+	
+	public MultiblockStructure(String type)
+	{
+		this.type = type;
+	}
+
+	public String getType()
+	{
+		return type;
+	}
+}
