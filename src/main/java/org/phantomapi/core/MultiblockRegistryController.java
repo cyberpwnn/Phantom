287c473182115c493f997ce1d4e976f3f1b8d94d
Daniel Mills <danielmillst@gmail.com>
2016-10-04 18:55:23 -0400

    Multiblock registry

---
+++ src/main/java/org/phantomapi/core/MultiblockRegistryController.java
@@ -0,0 +1,24 @@
+package org.phantomapi.core;
+
+import org.phantomapi.construct.Controllable;
+import org.phantomapi.construct.Controller;
+
+public class MultiblockRegistryController extends Controller
+{
+	public MultiblockRegistryController(Controllable parentController)
+	{
+		super(parentController);
+	}
+
+	@Override
+	public void onStart()
+	{
+		
+	}
+
+	@Override
+	public void onStop()
+	{
+		
+	}
+}
