6e34c352bf5edd90b69b02d6f88817928e2010d6
Daniel Mills <danielmillst@gmail.com>
2016-10-03 21:41:03 -0400

    Simple join info

--- src/main/java/org/phantomapi/core/ProtocolController.java
+++ src/main/java/org/phantomapi/core/ProtocolController.java
@@ -1,9 +1,13 @@
 package org.phantomapi.core;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.phantomapi.construct.Controllable;
 import org.phantomapi.construct.Controller;
+import org.phantomapi.ext.Protocol;
 import org.phantomapi.nms.EntityHider;
 import org.phantomapi.nms.EntityHider.Policy;
+import org.phantomapi.sync.TaskLater;
 
 public class ProtocolController extends Controller
 {
@@ -28,5 +32,18 @@ public class ProtocolController extends Controller
 	public void onStop()
 	{
 		
+	}
+	
+	@EventHandler
+	public void on(PlayerJoinEvent e)
+	{
+		new TaskLater()
+		{
+			@Override
+			public void run()
+			{
+				w(e.getPlayer().getName() + " <> Protocol " + Protocol.getProtocol(e.getPlayer()));
+			}
+		};
 	}
 }
