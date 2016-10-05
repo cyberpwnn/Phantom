fa1b364050b75dec085ed373fa88209755daf0b0
Daniel Mills <danielmillst@gmail.com>
2016-10-05 17:54:36 -0400

    Multiblock registry

--- src/main/java/org/phantomapi/core/MultiblockRegistryController.java
+++ src/main/java/org/phantomapi/core/MultiblockRegistryController.java
@@ -5,17 +5,22 @@ import java.io.IOException;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.util.Vector;
 import org.phantomapi.construct.Controllable;
 import org.phantomapi.construct.Controller;
 import org.phantomapi.event.MultiblockConstructEvent;
 import org.phantomapi.event.MultiblockDestroyEvent;
+import org.phantomapi.event.MultiblockInteractEvent;
 import org.phantomapi.event.MultiblockLoadEvent;
 import org.phantomapi.event.MultiblockUnloadEvent;
 import org.phantomapi.event.NestChunkLoadEvent;
+import org.phantomapi.event.NestChunkUnloadEvent;
+import org.phantomapi.lang.GList;
 import org.phantomapi.lang.GMap;
 import org.phantomapi.lang.GSet;
 import org.phantomapi.multiblock.Multiblock;
@@ -23,18 +28,23 @@ import org.phantomapi.multiblock.MultiblockInstance;
 import org.phantomapi.multiblock.MultiblockStructure;
 import org.phantomapi.multiblock.MultiblockUtils;
 import org.phantomapi.nest.Nest;
+import org.phantomapi.statistics.Monitorable;
 import org.phantomapi.sync.TaskLater;
+import org.phantomapi.util.C;
 import org.phantomapi.util.ExceptionUtil;
+import org.phantomapi.util.F;
+import net.minecraft.server.v1_8_R3.Material;
 
 /**
  * Manages multiblock registries
  * 
  * @author cyberpwn
  */
-public class MultiblockRegistryController extends Controller
+public class MultiblockRegistryController extends Controller implements Monitorable
 {
 	private GMap<String, MultiblockStructure> structures;
 	private GMap<Integer, Multiblock> instances;
+	private int v = 0;
 	
 	public MultiblockRegistryController(Controllable parentController)
 	{
@@ -66,9 +76,10 @@ public class MultiblockRegistryController extends Controller
 		structures.remove(mb.getType());
 	}
 	
-	@EventHandler
-	public void on(ChunkUnloadEvent e)
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void on(NestChunkUnloadEvent e)
 	{
+		v--;
 		for(Integer i : instances.k())
 		{
 			if(instances.get(i).getChunks().contains(e.getChunk()))
@@ -89,52 +100,64 @@ public class MultiblockRegistryController extends Controller
 		}
 	}
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void on(NestChunkLoadEvent e)
 	{
-		GSet<Integer> ids = new GSet<Integer>();
-		
-		for(String i : e.getNestedChunk().keys())
+		v++;
+		if(Nest.getChunk(e.getChunk()) != null)
 		{
-			if(i.startsWith("mb") && i.contains("."))
+			GSet<Integer> ids = new GSet<Integer>();
+			
+			for(String i : Nest.getChunk(e.getChunk()).keys())
 			{
+				if(i.startsWith("mb") && i.contains("."))
+				{
+					try
+					{
+						String s = i.split("\\.")[1].replaceAll("i-", "");
+						Integer v = Integer.valueOf(s);
+						ids.add(v);
+					}
+					
+					catch(Exception ex)
+					{
+						
+					}
+				}
+			}
+			
+			for(Integer i : ids)
+			{
+				if(instances.containsKey(i))
+				{
+					continue;
+				}
+				
 				try
 				{
-					String s = i.split("\\.")[1].replaceAll("i-", "");
-					Integer v = Integer.valueOf(s);
-					ids.add(v);
+					File file = MultiblockUtils.getFile(e.getWorld(), i);
+					
+					if(file.exists())
+					{
+						try
+						{
+							Multiblock mb = MultiblockUtils.load(e.getWorld(), i);
+							instances.put(i, mb);
+							MultiblockLoadEvent mbl = new MultiblockLoadEvent(mb, e.getWorld());
+							callEvent(mbl);
+						}
+						
+						catch(Exception exx)
+						{
+							
+						}
+					}
 				}
 				
 				catch(Exception ex)
 				{
 					
 				}
-			}
-		}
-		
-		for(Integer i : ids)
-		{
-			if(instances.k().contains(i))
-			{
-				continue;
-			}
-			
-			try
-			{
-				File file = MultiblockUtils.getFile(e.getWorld(), i);
-				
-				if(file.exists())
-				{
-					Multiblock mb = MultiblockUtils.load(e.getWorld(), i);
-					instances.put(i, mb);
-					MultiblockLoadEvent mbl = new MultiblockLoadEvent(mb, e.getWorld());
-					callEvent(mbl);
-				}
-			}
-			
-			catch(Exception ex)
-			{
-				
 			}
 		}
 	}
@@ -147,6 +170,11 @@ public class MultiblockRegistryController extends Controller
 			@Override
 			public void run()
 			{
+				if(Nest.getChunk(e.getBlock().getChunk()) == null)
+				{
+					return;
+				}
+				
 				for(String i : structures.k())
 				{
 					GMap<Vector, Location> map = structures.get(i).match(e.getBlock().getLocation());
@@ -186,8 +214,91 @@ public class MultiblockRegistryController extends Controller
 	}
 	
 	@EventHandler
+	public void on(PlayerInteractEvent e)
+	{
+		try
+		{
+			if(!e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
+			{
+				return;
+			}
+			
+			if(((e.getItem() == null || e.getItem().getType().equals(Material.AIR))) || e.getPlayer().isSneaking())
+			{
+				for(Integer i : instances.k())
+				{
+					if(instances.get(i).getWorld().equals(e.getClickedBlock().getWorld()) && instances.get(i).contains(e.getClickedBlock().getLocation()))
+					{
+						MultiblockInteractEvent evt = new MultiblockInteractEvent(e.getPlayer(), instances.get(i), e.getPlayer().getWorld(), e.getAction());
+						callEvent(evt);
+						e.setCancelled(true);
+						return;
+					}
+				}
+			}
+			
+			new TaskLater()
+			{
+				@Override
+				public void run()
+				{
+					if(Nest.getChunk(e.getClickedBlock().getChunk()) == null)
+					{
+						return;
+					}
+					
+					for(String i : structures.k())
+					{
+						GMap<Vector, Location> map = structures.get(i).match(e.getClickedBlock().getLocation());
+						
+						if(map != null)
+						{
+							int id = MultiblockUtils.getNextID(e.getClickedBlock().getWorld());
+							Multiblock mb = new MultiblockInstance(id, i, map);
+							
+							try
+							{
+								MultiblockConstructEvent mbc = new MultiblockConstructEvent(e.getPlayer(), mb, mb.getWorld());
+								callEvent(mbc);
+								
+								if(!e.isCancelled())
+								{
+									MultiblockUtils.save(mb);
+									instances.put(id, mb);
+									
+									for(Chunk j : mb.getChunks())
+									{
+										Nest.getChunk(j).set("mb.i-" + mb.getId(), mb.getType());
+									}
+								}
+							}
+							
+							catch(IOException ex)
+							{
+								ExceptionUtil.print(ex);
+							}
+							
+							break;
+						}
+					}
+				}
+			};
+		}
+		
+		catch(Exception ex)
+		{
+			
+		}
+	}
+	
+	@EventHandler
 	public void on(BlockBreakEvent e)
 	{
+		if(Nest.getChunk(e.getBlock().getChunk()) == null)
+		{
+			return;
+		}
+		
 		for(Integer i : instances.k())
 		{
 			if(instances.get(i).getWorld().equals(e.getBlock().getWorld()) && instances.get(i).contains(e.getBlock().getLocation()))
@@ -213,5 +324,31 @@ public class MultiblockRegistryController extends Controller
 				}
 			}
 		}
+	}
+	
+	@Override
+	public String getMonitorableData()
+	{
+		return "Locks: " + C.LIGHT_PURPLE + F.f(v);
+	}
+	
+	public GList<Multiblock> getMultiblocks()
+	{
+		return instances.v();
+	}
+	
+	public GList<Multiblock> getMultiblocks(String type)
+	{
+		GList<Multiblock> bls = getMultiblocks();
+		
+		for(Multiblock i : bls.copy())
+		{
+			if(!type.equals(i.getType()))
+			{
+				bls.remove(i);
+			}
+		}
+		
+		return bls;
 	}
 }
