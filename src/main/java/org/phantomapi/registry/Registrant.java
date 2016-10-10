2323f66bf9ad40d70096e7e44f1227434cd8645b
Daniel Mills <danielmillst@gmail.com>
2016-10-10 06:35:52 -0400

    Registrant

---
+++ src/main/java/org/phantomapi/registry/Registrant.java
@@ -0,0 +1,11 @@
+package org.phantomapi.registry;
+
+/**
+ * Represents a registrant. A Registrant can be registered by a registrar
+ *
+ * @author cyberpwn
+ */
+public interface Registrant
+{
+	
+}
