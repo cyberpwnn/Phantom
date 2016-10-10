405b3144d6fce903b8df7a9f4d0e01a4e2e9718e
Daniel Mills <danielmillst@gmail.com>
2016-10-09 22:43:35 -0400

    Blocks utilities

---
+++ src/main/java/org/phantomapi/world/Blocks.java
@@ -0,0 +1,54 @@
+package org.phantomapi.world;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.phantomapi.Phantom;
+import org.phantomapi.block.BlockHandler;
+import org.phantomapi.lang.GList;
+
+/**
+ * Blocks utilities
+ * 
+ * @author cyberpwn
+ */
+public class Blocks
+{
+	/**
+	 * Can the player modify the given block based on region handlers
+	 * 
+	 * @param p
+	 *            the player
+	 * @param block
+	 *            the block
+	 * @return true if the player can mine or place a block at the given
+	 *         location
+	 */
+	public static boolean canModify(Player p, Block block)
+	{
+		return getModify(p, block).isEmpty();
+	}
+	
+	/**
+	 * Get all handlers
+	 * 
+	 * @return the handlers
+	 */
+	public static GList<BlockHandler> getHandlers()
+	{
+		return Phantom.instance().getBlockCheckController().getHandlers();
+	}
+	
+	/**
+	 * Get all preventing modifications due to the following handlers
+	 * 
+	 * @param p
+	 *            the player
+	 * @param block
+	 *            the block
+	 * @return the handlers that have cancelled the modification
+	 */
+	public static GList<BlockHandler> getModify(Player p, Block block)
+	{
+		return Phantom.instance().getBlockCheckController().canModify(p, block);
+	}
+}
