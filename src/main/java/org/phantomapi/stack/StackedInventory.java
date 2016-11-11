e7e1d3cd344fd1e9fe571ccb65608be388613e3d
Daniel Mills <danielmillst@gmail.com>
2016-11-10 22:20:54 -0500

    Stacked inv entity data

--- src/main/java/org/phantomapi/stack/StackedInventory.java
+++ src/main/java/org/phantomapi/stack/StackedInventory.java
@@ -1,7 +1,10 @@
 package org.phantomapi.stack;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
+import org.phantomapi.clust.DataCluster;
 import org.phantomapi.inventory.PhantomInventory;
 import org.phantomapi.lang.GMap;
 
@@ -13,13 +16,15 @@ public class StackedInventory extends PhantomInventory implements StackedPhantom
 	{
 		super(i);
 		
-		this.inventoryContents = new GMap<Integer, Stack>();
+		inventoryContents = new GMap<Integer, Stack>();
+		pull();
 	}
 	
+	@Override
 	public GMap<Integer, Stack> getStacks()
 	{
 		thrash();
-		
+		inventoryContents.clear();
 		for(int i = 0; i < 36; i++)
 		{
 			inventoryContents.put(i, new Stack(getItem(i)));
@@ -40,6 +45,7 @@ public class StackedInventory extends PhantomInventory implements StackedPhantom
 		return inventoryContents;
 	}
 	
+	@Override
 	public void setStacks(GMap<Integer, Stack> stacks)
 	{
 		thrash();
@@ -47,11 +53,13 @@ public class StackedInventory extends PhantomInventory implements StackedPhantom
 		thrash();
 	}
 	
+	@Override
 	public void setStack(int slot, Stack stack)
 	{
 		inventoryContents.put(slot, stack);
 	}
 	
+	@Override
 	public Stack getStack(int slot)
 	{
 		if(getItem(slot) != null)
@@ -65,13 +73,52 @@ public class StackedInventory extends PhantomInventory implements StackedPhantom
 		}
 	}
 	
+	@Override
 	public void thrash()
 	{
 		for(int i : inventoryContents.k())
 		{
 			setItem(i, inventoryContents.get(i).toItemStack());
 		}
+	}
+	
+	@Override
+	public byte[] toData() throws IOException
+	{
+		DataCluster cc = new DataCluster();
 		
+		for(Integer i : inventoryContents.k())
+		{
+			cc.set("i-" + i, inventoryContents.get(i).toData() + "");
+		}
+		
+		return cc.compress();
+	}
+	
+	@Override
+	public void fromData(byte[] data) throws IOException
+	{
+		DataCluster cc = new DataCluster(data);
 		inventoryContents.clear();
+		
+		for(String i : cc.keys())
+		{
+			Integer l = Integer.valueOf(i.split("-")[1]);
+			
+			Stack v = new Stack(Material.GLASS);
+			v.fromData(cc.getString(i).getBytes(Charset.defaultCharset()));
+			inventoryContents.put(l, v);
+		}
+	}
+	
+	@Override
+	public void pull()
+	{
+		inventoryContents.clear();
+		
+		for(int i = 0; i < 36; i++)
+		{
+			inventoryContents.put(i, new Stack(getItem(i)));
+		}
 	}
 }
