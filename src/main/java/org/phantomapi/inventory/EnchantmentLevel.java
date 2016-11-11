89624219af97027a686f348bd89aba8e1e5cb260
Daniel Mills <danielmillst@gmail.com>
2016-11-10 21:39:56 -0500

    Enchantment level entity

--- src/main/java/org/phantomapi/inventory/EnchantmentLevel.java
+++ src/main/java/org/phantomapi/inventory/EnchantmentLevel.java
@@ -1,8 +1,11 @@
 package org.phantomapi.inventory;
 
+import java.io.IOException;
 import org.bukkit.enchantments.Enchantment;
+import org.phantomapi.clust.DataCluster;
+import org.phantomapi.clust.DataEntity;
 
-public class EnchantmentLevel
+public class EnchantmentLevel implements DataEntity
 {
 	private Enchantment enchantment;
 	private Integer level;
@@ -17,27 +20,28 @@ public class EnchantmentLevel
 	{
 		this(enchantment, 1);
 	}
-
+	
 	public Enchantment getEnchantment()
 	{
 		return enchantment;
 	}
-
+	
 	public void setEnchantment(Enchantment enchantment)
 	{
 		this.enchantment = enchantment;
 	}
-
+	
 	public Integer getLevel()
 	{
 		return level;
 	}
-
+	
 	public void setLevel(Integer level)
 	{
 		this.level = level;
 	}
 	
+	@Override
 	public boolean equals(Object o)
 	{
 		if(o instanceof EnchantmentLevel)
@@ -48,5 +52,27 @@ public class EnchantmentLevel
 		}
 		
 		return false;
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public byte[] toData() throws IOException
+	{
+		DataCluster cc = new DataCluster();
+		
+		cc.set("e", enchantment.getId());
+		cc.set("l", level);
+		
+		return cc.compress();
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void fromData(byte[] data) throws IOException
+	{
+		DataCluster cc = new DataCluster(data);
+		
+		enchantment = Enchantment.getById(cc.getInt("e"));
+		level = cc.getInt("l");
 	}
 }
