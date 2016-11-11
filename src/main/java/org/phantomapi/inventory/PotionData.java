4a30eebc19fe0df138dc70fe7942216313997177
Daniel Mills <danielmillst@gmail.com>
2016-11-10 21:39:49 -0500

    Potion data data entity

--- src/main/java/org/phantomapi/inventory/PotionData.java
+++ src/main/java/org/phantomapi/inventory/PotionData.java
@@ -1,9 +1,14 @@
 package org.phantomapi.inventory;
 
+import java.io.IOException;
 import java.util.Collection;
 import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.phantomapi.clust.DataCluster;
+import org.phantomapi.clust.DataEntity;
+import org.phantomapi.lang.GList;
 
-public class PotionData
+public class PotionData implements DataEntity
 {
 	private boolean splash;
 	private int level;
@@ -44,5 +49,40 @@ public class PotionData
 	public void setEffects(Collection<PotionEffect> effects)
 	{
 		this.effects = effects;
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public byte[] toData() throws IOException
+	{
+		DataCluster cc = new DataCluster();
+		GList<String> e = new GList<String>();
+		
+		for(PotionEffect i : effects)
+		{
+			e.add(i.getType().getId() + ";" + i.getAmplifier() + ";" + i.getDuration());
+		}
+		
+		cc.set("s", splash);
+		cc.set("l", level);
+		cc.set("e", e);
+		
+		return cc.compress();
+	}
+	
+	@Override
+	public void fromData(byte[] data) throws IOException
+	{
+		DataCluster cc = new DataCluster(data);
+		splash = cc.getBoolean("s");
+		level = cc.getInt("l");
+		effects.clear();
+		
+		for(String i : cc.getStringList("e"))
+		{
+			@SuppressWarnings("deprecation")
+			PotionEffect pe = new PotionEffect(PotionEffectType.getById(Integer.valueOf(i.split(";")[0])), Integer.valueOf(i.split(";")[2]), Integer.valueOf(i.split(";")[1]));
+			effects.add(pe);
+		}
 	}
 }
