26e578fc88fe46e5d87a38387f1d425a39e5a34a
Daniel Mills <danielmillst@gmail.com>
2016-11-11 20:00:12 -0500

    Package info

---
+++ src/main/java/org/phantomapi/chromatic/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Chomatic blocks which supply face-specific coloring and alpha layering for
+ * blending along with what color is seen from a given location
+ */
+package org.phantomapi.chromatic;
