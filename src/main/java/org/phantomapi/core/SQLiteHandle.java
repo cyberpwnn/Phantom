0c36c913bedf16959689b77f3161bf85091209e1
Daniel Mills <danielmillst@gmail.com>
2016-11-12 21:19:52 -0500

    SQLite handle

---
+++ src/main/java/org/phantomapi/core/SQLiteHandle.java
@@ -0,0 +1,66 @@
+package org.phantomapi.core;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.phantomapi.clust.SQLite;
+
+public class SQLiteHandle
+{
+	private SQLite db;
+	private File file;
+	private Connection connection;
+	
+	public SQLiteHandle(File file)
+	{
+		this.file = file;
+		db = new SQLite(file);
+		connection = null;
+	}
+	
+	public Connection getConnection() throws ClassNotFoundException, SQLException
+	{
+		if(connection == null || connection.isClosed())
+		{
+			connection = db.openConnection();
+		}
+		
+		return connection;
+	}
+	
+	public void close() throws SQLException
+	{
+		if(connection != null && !connection.isClosed())
+		{
+			connection.close();
+		}
+	}
+	
+	public SQLite getDb()
+	{
+		return db;
+	}
+	
+	public File getFile()
+	{
+		return file;
+	}
+	
+	public boolean isClosed()
+	{
+		if(connection != null)
+		{
+			try
+			{
+				return connection.isClosed();
+			}
+			
+			catch(SQLException e)
+			{
+				
+			}
+		}
+		
+		return true;
+	}
+}
