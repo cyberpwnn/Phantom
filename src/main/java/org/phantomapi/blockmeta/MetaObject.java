edaf1b71eea3e7dd2b8bcc34aa9866817d72f9aa
Daniel Mills <danielmillst@gmail.com>
2016-11-14 11:54:38 -0500

    Base meta object

---
+++ src/main/java/org/phantomapi/blockmeta/MetaObject.java
@@ -0,0 +1,11 @@
+package org.phantomapi.blockmeta;
+
+import org.phantomapi.clust.ConfigurableObject;
+
+public class MetaObject extends ConfigurableObject
+{
+	public MetaObject(String codeName)
+	{
+		super(codeName);
+	}
+}
