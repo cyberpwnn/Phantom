2c68a966bf40b74c86c41ede95902de2f251bf16
Daniel Mills <danielmillst@gmail.com>
2016-11-14 15:43:36 -0500

    Re-add fq

--- src/main/java/org/phantomapi/core/ProtocolController.java
+++ src/main/java/org/phantomapi/core/ProtocolController.java
@@ -4,14 +4,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.phantomapi.construct.Controllable;
 import org.phantomapi.construct.Controller;
+import org.phantomapi.event.EquipmentUpdateEvent;
 import org.phantomapi.ext.Protocol;
 import org.phantomapi.nms.EntityHider;
 import org.phantomapi.nms.EntityHider.Policy;
+import org.phantomapi.nms.FakeEquipment;
 import org.phantomapi.sync.TaskLater;
 
 public class ProtocolController extends Controller
 {
 	private EntityHider entityHider;
+	private FakeEquipment fakeEquipment;
 	
 	public ProtocolController(Controllable parentController)
 	{
@@ -22,6 +25,26 @@ public class ProtocolController extends Controller
 	public void onStart()
 	{
 		entityHider = new EntityHider(getPlugin(), Policy.BLACKLIST);
+		
+		fakeEquipment = new FakeEquipment(getPlugin())
+		{
+			@Override
+			protected boolean onEquipmentSending(EquipmentSendingEvent e)
+			{
+				EquipmentUpdateEvent ee = new EquipmentUpdateEvent(e.getVisibleEntity(), e.getEquipment(), e.getClient(), e.getSlot());
+				callEvent(ee);
+				
+				if(ee.isCancelled())
+				{
+					return false;
+				}
+				
+				e.setEquipment(ee.getItem());
+				e.setSlot(ee.getSlot());
+				
+				return true;
+			}
+		};
 	}
 	
 	public EntityHider getHider()
@@ -46,5 +69,10 @@ public class ProtocolController extends Controller
 				w(e.getPlayer().getName() + " <> Protocol " + Protocol.getProtocol(e.getPlayer()));
 			}
 		};
+	}
+	
+	public FakeEquipment getFakeEquipment()
+	{
+		return fakeEquipment;
 	}
 }
