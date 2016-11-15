721781dafdb9e45f81afdbfc6303bb5bc097c924
Daniel Mills <danielmillst@gmail.com>
2016-11-15 04:23:43 -0500

    Block meta

---
+++ src/main/java/org/phantomapi/blockmeta/BlockMeta.java
@@ -0,0 +1,41 @@
+package org.phantomapi.blockmeta;
+
+import org.phantomapi.clust.Configurable;
+import org.phantomapi.clust.DataCluster;
+import org.phantomapi.clust.LinkedDataCluster;
+
+public class BlockMeta implements Configurable
+{
+	private LinkedDataCluster cc;
+	private String codeName;
+	
+	public BlockMeta(String codeName, LinkedDataCluster cc)
+	{
+		this.cc = cc;
+		this.codeName = codeName;
+	}
+	
+	@Override
+	public void onNewConfig()
+	{
+		
+	}
+	
+	@Override
+	public void onReadConfig()
+	{
+		
+	}
+	
+	@Override
+	public DataCluster getConfiguration()
+	{
+		return cc;
+	}
+	
+	@Override
+	public String getCodeName()
+	{
+		return codeName;
+	}
+}
