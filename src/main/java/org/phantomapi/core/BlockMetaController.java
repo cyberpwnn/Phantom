dfc73f7fad136a784cf825c9c9d652cdc63c7e5d
Daniel Mills <danielmillst@gmail.com>
2016-11-15 03:45:11 -0500

    Remove meta

--- src/main/java/org/phantomapi/core/BlockMetaController.java
+++ src/main/java/org/phantomapi/core/BlockMetaController.java
@@ -3,28 +3,29 @@ package org.phantomapi.core;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
-import org.phantomapi.async.A;
-import org.phantomapi.blockmeta.ChunkMeta;
 import org.phantomapi.construct.Controllable;
 import org.phantomapi.construct.Controller;
-import org.phantomapi.event.MetaChunkLoadEvent;
-import org.phantomapi.event.MetaChunkUnloadEvent;
-import org.phantomapi.lang.GChunk;
-import org.phantomapi.lang.GMap;
+import org.phantomapi.construct.Ticked;
 import org.phantomapi.statistics.Monitorable;
-import org.phantomapi.sync.S;
-import org.phantomapi.util.C;
-import org.phantomapi.util.F;
 
+@Ticked(5)
 public class BlockMetaController extends Controller implements Monitorable
 {
-	private GMap<GChunk, ChunkMeta> metaCache;
-	
 	public BlockMetaController(Controllable parentController)
 	{
 		super(parentController);
+	}
+	
+	@EventHandler
+	public void on(ChunkLoadEvent e)
+	{
 		
-		metaCache = new GMap<GChunk, ChunkMeta>();
+	}
+	
+	@EventHandler
+	public void on(ChunkUnloadEvent e)
+	{
+		
 	}
 	
 	@Override
@@ -36,65 +37,17 @@ public class BlockMetaController extends Controller implements Monitorable
 	@Override
 	public void onStop()
 	{
-		
 	}
 	
-	@EventHandler
-	public void on(ChunkLoadEvent e)
+	@Override
+	public void onTick()
 	{
-		GChunk chunk = new GChunk(e.getChunk());
 		
-		new A()
-		{
-			@Override
-			public void async()
-			{
-				ChunkMeta cm = new ChunkMeta(chunk);
-				loadSqLite(cm, cm.getFile());
-				
-				new S()
-				{
-					@Override
-					public void sync()
-					{
-						metaCache.put(chunk, cm);
-						callEvent(new MetaChunkLoadEvent(chunk.toChunk(), cm));
-					}
-				};
-			}
-		};
-	}
-	
-	@EventHandler
-	public void on(ChunkUnloadEvent e)
-	{
-		GChunk chunk = new GChunk(e.getChunk());
-		
-		if(!metaCache.containsKey(chunk))
-		{
-			return;
-		}
-		
-		ChunkMeta cm = metaCache.get(chunk);
-		cm.getConfiguration().flushLinks();
-		callEvent(new MetaChunkUnloadEvent(e.getChunk(), cm));
-		cm.getConfiguration().flushLinks();
-		
-		metaCache.remove(chunk);
-		
-		new A()
-		{
-			@Override
-			public void async()
-			{
-				saveSqLite(cm, cm.getFile());
-			}
-		};
 	}
 	
 	@Override
 	public String getMonitorableData()
 	{
-		return "Chunks: " + C.LIGHT_PURPLE + F.f(metaCache.size());
+		return "";
 	}
 }
