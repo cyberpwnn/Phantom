063b22b134ff0170e5b8adaa2ab47596d7c8b996
Daniel Mills <danielmillst@gmail.com>
2016-11-16 04:02:57 -0500

    Fix placeholders

--- src/main/java/org/phantomapi/placeholder/PlaceholderUtil.java
+++ src/main/java/org/phantomapi/placeholder/PlaceholderUtil.java
@@ -3,8 +3,22 @@ package org.phantomapi.placeholder;
 import org.bukkit.entity.Player;
 import me.clip.placeholderapi.PlaceholderAPI;
 
+/**
+ * Placeholder utils
+ * 
+ * @author cyberpwn
+ */
 public class PlaceholderUtil
 {
+	/**
+	 * Handle
+	 * 
+	 * @param p
+	 *            the player
+	 * @param s
+	 *            the string
+	 * @return the placeholder result or null
+	 */
 	public static String handle(Player p, String s)
 	{
 		return PlaceholderAPI.setPlaceholders(p, s);
