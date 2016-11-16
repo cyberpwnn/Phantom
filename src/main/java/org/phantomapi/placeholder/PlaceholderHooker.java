2f50b9905cbf2052bef489eda91add91d7d2191a
Daniel Mills <danielmillst@gmail.com>
2016-11-16 04:28:58 -0500

    Number formatting

--- src/main/java/org/phantomapi/placeholder/PlaceholderHooker.java
+++ src/main/java/org/phantomapi/placeholder/PlaceholderHooker.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.phantomapi.Phantom;
 import org.phantomapi.nms.NMSX;
+import org.phantomapi.util.F;
 import me.clip.placeholderapi.external.EZPlaceholderHook;
 
 public class PlaceholderHooker extends EZPlaceholderHook
@@ -28,17 +29,67 @@ public class PlaceholderHooker extends EZPlaceholderHook
 				return (Phantom.getNetworkCount() - Phantom.instance().getWraithController().getCount()) + "";
 			}
 			
-			for(String i : Phantom.getServers())
-			{
-				if(q.equalsIgnoreCase("server_" + i.toLowerCase() + "_count"))
-				{
-					return Phantom.getNetworkCount(i) + "";
-				}
-			}
-			
 			if(q.equalsIgnoreCase("ping"))
 			{
 				return NMSX.ping(p) + "ms";
+			}
+			
+			if(q.equalsIgnoreCase("bad_decimal"))
+			{
+				return "" + Math.random() * 1234.574;
+			}
+			
+			if(q.equalsIgnoreCase("bad_percent"))
+			{
+				return "" + Math.random();
+			}
+			
+			if(q.equalsIgnoreCase("bad_number"))
+			{
+				return "" + (int) (Math.random() * 1234534374);
+			}
+			
+			if(q.contains("filter-") && q.contains("@") && q.split("@").length == 2)
+			{
+				String placeholder = q.split("@")[1];
+				String result = PlaceholderUtil.handle(p, placeholder);
+				Double v = Double.valueOf(result);
+				
+				if(q.startsWith("filter-dec") || q.startsWith("filter-per"))
+				{
+					Integer num = Integer.valueOf(q.split("@")[0].substring(10));
+					
+					if(q.startsWith("filter-dec"))
+					{
+						return F.f(v, num);
+					}
+					
+					if(q.startsWith("filter-per"))
+					{
+						return F.pc(v, num);
+					}
+				}
+				
+				else if(q.startsWith("filter-num"))
+				{
+					return F.f(v);
+				}
+			}
+			
+			try
+			{
+				for(String i : Phantom.getServers())
+				{
+					if(q.equalsIgnoreCase("server_" + i.toLowerCase() + "_count"))
+					{
+						return Phantom.getNetworkCount(i) + "";
+					}
+				}
+			}
+			
+			catch(Exception ee)
+			{
+				
 			}
 		}
 		
