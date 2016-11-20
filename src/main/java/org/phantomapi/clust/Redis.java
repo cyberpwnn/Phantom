83d2d16a0d5cb4e512cfd0c8561cc93b7a1e25c6
Daniel Mills <danielmillst@gmail.com>
2016-11-20 04:47:28 -0500

    Type change

--- src/main/java/org/phantomapi/clust/Redis.java
+++ src/main/java/org/phantomapi/clust/Redis.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
  * 
  * @author cyberpwn
  */
-@Target(ElementType.FIELD)
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Redis
 {
