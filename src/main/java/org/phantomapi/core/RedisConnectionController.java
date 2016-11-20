79936a459021250817cea48b8fab4ae9bf207787
Daniel Mills <danielmillst@gmail.com>
2016-11-20 04:17:12 -0500

    Redis connection contreoller

---
+++ src/main/java/org/phantomapi/core/RedisConnectionController.java
@@ -0,0 +1,84 @@
+package org.phantomapi.core;
+
+import org.phantomapi.clust.ConfigurableController;
+import org.phantomapi.clust.Keyed;
+import org.phantomapi.construct.Controllable;
+import redis.clients.jedis.Jedis;
+
+public class RedisConnectionController extends ConfigurableController
+{
+	@Keyed("redis.address")
+	public String address = "???";
+	
+	@Keyed("redis.port")
+	public int port = 6379;
+	
+	@Keyed("redis.password")
+	public String password = "redis is bae";
+	
+	private Jedis r;
+	
+	public RedisConnectionController(Controllable parentController)
+	{
+		super(parentController, "redis");
+	}
+	
+	public void connect()
+	{
+		if(isConnected())
+		{
+			s("Reconnecting to redis");
+			r.disconnect();
+		}
+		
+		try
+		{
+			r = new Jedis(address, port);
+			r.auth(password);
+			s("Connected to redis");
+		}
+		
+		catch(Exception e)
+		{
+			f("Failed to connect to redis.");
+			f(e.getMessage());
+		}
+	}
+	
+	public void write(String key, String value)
+	{
+		r.set(key, value);
+	}
+	
+	public String read(String key)
+	{
+		return r.get(key);
+	}
+	
+	public boolean isConnected()
+	{
+		return r != null && r.isConnected();
+	}
+	
+	@Override
+	public void onStart()
+	{
+		loadCluster(this);
+	}
+	
+	@Override
+	public void onStop()
+	{
+		if(isConnected())
+		{
+			s("Disconnecting");
+			r.disconnect();
+		}
+	}
+	
+	@Override
+	public void onReadConfig()
+	{
+		connect();
+	}
+}
