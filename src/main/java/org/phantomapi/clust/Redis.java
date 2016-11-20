232b5b3dab5a750d8376467fc370c13ad3295c59
Daniel Mills <danielmillst@gmail.com>
2016-11-20 04:29:08 -0500

    Redis annotation

---
+++ src/main/java/org/phantomapi/clust/Redis.java
@@ -0,0 +1,18 @@
+package org.phantomapi.clust;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Define a redis prefix
+ * 
+ * @author cyberpwn
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Redis
+{
+	public String value();
+}
