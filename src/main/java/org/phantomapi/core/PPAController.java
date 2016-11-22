0c2cb11a9435ef37df20d72c510df25867a82139
Daniel Mills <danielmillst@gmail.com>
2016-11-21 19:54:46 -0500

    Handle ppas

--- src/main/java/org/phantomapi/core/PPAController.java
+++ src/main/java/org/phantomapi/core/PPAController.java
@@ -7,23 +7,35 @@ import org.phantomapi.clust.ConfigurableController;
 import org.phantomapi.clust.Keyed;
 import org.phantomapi.construct.Controllable;
 import org.phantomapi.construct.Ticked;
+import org.phantomapi.lang.GList;
+import org.phantomapi.ppa.PPA;
+import org.phantomapi.ppa.PPAP;
+import org.phantomapi.statistics.Monitorable;
+import org.phantomapi.sync.S;
+import org.phantomapi.util.C;
+import org.phantomapi.util.M;
 import redis.clients.jedis.Jedis;
 
-@Ticked(0)
-public class PPAController extends ConfigurableController
+@Ticked(20)
+public class PPAController extends ConfigurableController implements Monitorable
 {
 	@Keyed("identifier")
 	@Comment("The identifier for this server.\nUse a human readable name such as the server name.")
 	public String id = "null";
 	
+	private GList<PPA> out;
 	private Boolean running;
 	private Jedis r;
+	private int in = 0;
+	private int outt = 0;
+	private int wait = 0;
 	
 	public PPAController(Controllable parentController)
 	{
 		super(parentController, "ppa");
 		
 		running = false;
+		out = new GList<PPA>();
 	}
 	
 	@Override
@@ -39,6 +51,8 @@ public class PPAController extends ConfigurableController
 	{
 		if(!running)
 		{
+			GList<PPA> o = out.copy();
+			out.clear();
 			running = true;
 			
 			new A()
@@ -46,15 +60,101 @@ public class PPAController extends ConfigurableController
 				@Override
 				public void async()
 				{
+					PPAP p = new PPAP();
+					String s = r.get("ppa:inbox");
+					GList<PPA> h = new GList<PPA>();
+					in = o.size();
 					
+					if(s != null)
+					{
+						p.setData(s);
+					}
+					
+					for(PPA i : o)
+					{
+						p.add(i);
+					}
+					
+					for(PPA i : p.getPPAS().copy())
+					{
+						if(i.getDestination().equals(id) || i.getDestination().equals("all"))
+						{
+							h.add(i);
+							p.getPPAS().remove(i);
+						}
+					}
+					
+					outt = h.size();
+					
+					for(PPA i : p.getPPAS().copy())
+					{
+						if((M.ms() - i.getTime()) > 10000)
+						{
+							f("Packet Timed Out: " + i.toJSON().toString());
+							p.getPPAS().remove(i);
+						}
+					}
+					
+					if(!p.getPPAS().isEmpty())
+					{
+						r.set("ppa:inbox", p.getData());
+					}
+					
+					else
+					{
+						r.del("ppa:inbox");
+					}
+					
+					wait = p.getPPAS().size();
+					
+					if(!h.isEmpty())
+					{
+						new S()
+						{
+							@Override
+							public void sync()
+							{
+								handle(h);
+							}
+						};
+					}
+					
+					new S()
+					{
+						@Override
+						public void sync()
+						{
+							running = false;
+						}
+					};
 				}
 			};
 		}
+	}
+	
+	public void handle(GList<PPA> handles)
+	{
+		for(PPA i : handles)
+		{
+			s(i.getDestination() + " -> " + i.getSource() + " : " + i.toJSON().toString());
+		}
+	}
+	
+	public void send(PPA ppa)
+	{
+		w(ppa.getSource() + " -> " + ppa.getDestination() + " : " + ppa.toJSON().toString());
+		out.add(ppa);
 	}
 	
 	@Override
 	public void onStop()
 	{
 		r.disconnect();
+	}
+	
+	@Override
+	public String getMonitorableData()
+	{
+		return C.GREEN + "> " + in + C.YELLOW + " > " + wait + " > " + C.RED + outt + " >";
 	}
 }
