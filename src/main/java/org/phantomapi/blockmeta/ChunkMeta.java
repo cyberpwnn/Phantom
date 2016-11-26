967f5c76a6b1cba7d72e806cbd73e1e251dc4610
Daniel Mills <danielmillst@gmail.com>
2016-11-26 18:04:32 -0500

    Chunk meta scehamatics

--- src/main/java/org/phantomapi/blockmeta/ChunkMeta.java
+++ src/main/java/org/phantomapi/blockmeta/ChunkMeta.java
@@ -8,12 +8,23 @@ import org.phantomapi.lang.GChunk;
 import org.phantomapi.lang.GList;
 import org.phantomapi.util.Worlds;
 
+/**
+ * Represents a meta host which holds meta blocks
+ * 
+ * @author cyberpwn
+ */
 public class ChunkMeta implements Configurable
 {
 	private GChunk gc;
 	private DataCluster cc;
 	private String codeName;
 	
+	/**
+	 * Create a chunk meta object
+	 * 
+	 * @param gc
+	 *            the gchunk
+	 */
 	public ChunkMeta(GChunk gc)
 	{
 		this.gc = gc;
@@ -45,16 +56,31 @@ public class ChunkMeta implements Configurable
 		return codeName;
 	}
 	
+	/**
+	 * Get the file name this would be saved to
+	 * 
+	 * @return the file
+	 */
 	public File getFile()
 	{
 		return new File(new File(Worlds.getWorld(gc.getWorld()).getWorldFolder(), "meta"), "r." + gc.getX() + "." + gc.getZ() + ".pma");
 	}
 	
+	/**
+	 * Does this chunk have any data other than defaults?
+	 * 
+	 * @return true if it has data which would be lost if not saved
+	 */
 	public boolean hasData()
 	{
 		return !getConfiguration().keys().isEmpty();
 	}
 	
+	/**
+	 * Get all blocks in this chunk which have meta
+	 * 
+	 * @return the meta blocks
+	 */
 	public GList<Block> getBlocks()
 	{
 		GList<String> roots = new GList<String>(getConfiguration().getRoots("blocks"));
@@ -69,6 +95,15 @@ public class ChunkMeta implements Configurable
 		return blocks;
 	}
 	
+	/**
+	 * Get a meta block. If it exists, and already has data, it will be
+	 * returned. If it does not exist, a new linked meta instance will be
+	 * returned.
+	 * 
+	 * @param block
+	 *            the block
+	 * @return the block meta or null if it does not fall within this chunk
+	 */
 	public BlockMeta getBlock(Block block)
 	{
 		if(gc.getWorld().equals(block.getWorld().getName()) && gc.getX() == block.getChunk().getX() && gc.getZ() == block.getChunk().getZ())
