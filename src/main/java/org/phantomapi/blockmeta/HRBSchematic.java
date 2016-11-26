bacc5397a2ba06fa0b3702330323a532f7a0916f
Daniel Mills <danielmillst@gmail.com>
2016-11-26 17:37:58 -0500

    HRB Schematic data

--- src/main/java/org/phantomapi/blockmeta/HRBSchematic.java
+++ src/main/java/org/phantomapi/blockmeta/HRBSchematic.java
@@ -2,7 +2,9 @@ package org.phantomapi.blockmeta;
 
 import java.io.IOException;
 import org.phantomapi.clust.ConfigurableObject;
+import org.phantomapi.clust.DataCluster;
 import org.phantomapi.clust.DataEntity;
+import org.phantomapi.clust.JSONObject;
 import org.phantomapi.world.VectorSchematic;
 
 public class HRBSchematic extends ConfigurableObject implements DataEntity
@@ -35,12 +37,25 @@ public class HRBSchematic extends ConfigurableObject implements DataEntity
 		String config = getConfiguration().toJSON().toString();
 		String meta = getMeta().getConfiguration().toJSON().toString();
 		String schematic = getSchematic().toConfiguration().toJSON().toString();
+		DataCluster cc = new DataCluster();
+		cc.set("c", config);
+		cc.set("m", meta);
+		cc.set("s", schematic);
+		
+		return cc.compress();
 	}
 	
 	@Override
 	public void fromData(byte[] data) throws IOException
 	{
-		// TODO Auto-generated method stub
-		
+		DataCluster cc = new DataCluster(data);
+		String config = cc.getString("c");
+		String meta = cc.getString("m");
+		String schematic = cc.getString("s");
+		getConfiguration().clear();
+		getConfiguration().addJson(new JSONObject(config));
+		getMeta().getConfiguration().clear();
+		getMeta().getConfiguration().addJson(new JSONObject(meta));
+		getSchematic().fromConfiguration(new DataCluster(new JSONObject(schematic)));
 	}
 }
