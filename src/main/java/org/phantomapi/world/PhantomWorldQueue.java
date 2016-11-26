d2c4f67afbd7554016063e13ad168b4f22415bd0
Daniel Mills <danielmillst@gmail.com>
2016-11-26 17:28:54 -0500

    World queue shapes and biomes

--- src/main/java/org/phantomapi/world/PhantomWorldQueue.java
+++ src/main/java/org/phantomapi/world/PhantomWorldQueue.java
@@ -3,6 +3,7 @@ package org.phantomapi.world;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Biome;
 import org.phantomapi.lang.GMap;
 
 /**
@@ -47,6 +48,61 @@ public class PhantomWorldQueue
 	public void set(Location location, Material material)
 	{
 		getSession(location.getWorld()).set(location, material);
+	}
+	
+	public void makePyramid(Location center, MaterialBlock mb, int height, boolean filled)
+	{
+		getSession(center.getWorld()).makePyramid(center, mb, height, filled);
+	}
+	
+	public void makePyramid(Location center, VariableBlock mb, int height, boolean filled)
+	{
+		getSession(center.getWorld()).makePyramid(center, mb, height, filled);
+	}
+	
+	public void makeSphere(Location center, MaterialBlock mb, double radiusX, double radiusY, double radiusZ, boolean filled)
+	{
+		getSession(center.getWorld()).makeSphere(center, mb, radiusX, radiusY, radiusZ, filled);
+	}
+	
+	public void makeSphere(Location center, VariableBlock mb, double radiusX, double radiusY, double radiusZ, boolean filled)
+	{
+		getSession(center.getWorld()).makeSphere(center, mb, radiusX, radiusY, radiusZ, filled);
+	}
+	
+	public void makeSphere(Location center, MaterialBlock mb, double radius, boolean filled)
+	{
+		getSession(center.getWorld()).makeSphere(center, mb, radius, filled);
+	}
+	
+	public void makeSphere(Location center, VariableBlock mb, double radius, boolean filled)
+	{
+		getSession(center.getWorld()).makeSphere(center, mb, radius, filled);
+	}
+	
+	public void makeCylinder(Location center, MaterialBlock mb, double radius, int height, boolean filled)
+	{
+		getSession(center.getWorld()).makeCylinder(center, mb, radius, height, filled);
+	}
+	
+	public void makeCylinder(Location center, VariableBlock mb, double radius, int height, boolean filled)
+	{
+		getSession(center.getWorld()).makeCylinder(center, mb, radius, height, filled);
+	}
+	
+	public Biome getBiome(Location location)
+	{
+		return getSession(location.getWorld()).getBiome(location.getBlockX(), location.getBlockZ());
+	}
+	
+	public void setBiome(Location location, Biome biome)
+	{
+		getSession(location.getWorld()).setBiome(location.getBlockX(), location.getBlockZ(), biome);
+	}
+	
+	public int getHighestBlock(Location location)
+	{
+		return getSession(location.getWorld()).getHighestBlock(location.getBlockX(), location.getBlockZ());
 	}
 	
 	public MaterialBlock get(Location location)
