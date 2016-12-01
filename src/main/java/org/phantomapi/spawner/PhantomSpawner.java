b82ecf167a9321a0bd8ce3a77abb517505977bbd
Daniel Mills <danielmillst@gmail.com>
2016-11-30 23:43:15 -0500

    Phbantom spawners

---
+++ src/main/java/org/phantomapi/spawner/PhantomSpawner.java
@@ -0,0 +1,40 @@
+package org.phantomapi.spawner;
+
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
+
+public class PhantomSpawner
+{
+	private Block block;
+	
+	public PhantomSpawner(Block block)
+	{
+		this.block = block;
+	}
+	
+	public void setType(EntityType type)
+	{
+		CreatureSpawner c = (CreatureSpawner) block.getState();
+		c.setSpawnedType(type);
+		
+		try
+		{
+			Class<?> ct = Class.forName("org.bukkit.entity.CreatureType");
+			Object ctype = ct.getMethod("fromEntityType", EntityType.class).invoke(null, type);
+			c.getClass().getMethod("setCreatureType", ct).invoke(c, ctype);
+		}
+		
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public EntityType getType()
+	{
+		CreatureSpawner c = (CreatureSpawner) block.getState();
+		
+		return c.getSpawnedType();
+	}
+}
