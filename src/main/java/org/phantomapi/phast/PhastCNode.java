207a3b1249e5b258e4bcf75c6fe769efb40715c7
Daniel Mills <danielmillst@gmail.com>
2016-12-05 20:46:37 -0500

    Support iteself

--- src/main/java/org/phantomapi/phast/PhastCNode.java
+++ src/main/java/org/phantomapi/phast/PhastCNode.java
@@ -1,6 +1,8 @@
 package org.phantomapi.phast;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.server.ServerCommandEvent;
+import org.phantomapi.Phantom;
 import org.phantomapi.lang.GList;
 
 /**
@@ -25,6 +27,7 @@ public class PhastCNode extends PhastNode
 	public void on(String[] args)
 	{
 		String command = new GList<String>(args).toString(" ");
+		Phantom.instance().callEvent(new ServerCommandEvent(Bukkit.getConsoleSender(), command));
 		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
 	}
 }
