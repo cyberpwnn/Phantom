92ad57a107916ccf86068b36803cd8269af04191
Daniel Mills <danielmillst@gmail.com>
2016-12-24 15:48:53 -0500

    Hyve jd

--- src/main/java/org/phantomapi/hive/Hyve.java
+++ src/main/java/org/phantomapi/hive/Hyve.java
@@ -12,20 +12,48 @@ import org.phantomapi.clust.Configurable;
  */
 public interface Hyve extends Configurable
 {
+	/**
+	 * Get the type of this hyve
+	 * 
+	 * @return the hyve type
+	 */
 	public HyveType getType();
 	
+	/**
+	 * Save changes to its destination
+	 */
 	public void save();
 	
+	/**
+	 * Load any data and create defaults
+	 */
 	public void load();
 	
+	/**
+	 * Delete all references of this hyve
+	 */
 	public void drop();
 	
+	/**
+	 * Check if there is hyve data on the given object
+	 * 
+	 * @param block
+	 *            the block
+	 * @return true if data exists
+	 */
 	public static boolean exists(Block block)
 	{
 		File file = new File(block.getWorld().getWorldFolder(), "block");
 		return new File(file, "hyve-block." + block.getX() + "." + block.getY() + "." + block.getZ() + ".hy").exists();
 	}
 	
+	/**
+	 * Check if there is hyve data on the given object
+	 * 
+	 * @param chunk
+	 *            the chunk
+	 * @return true if data exists
+	 */
 	public static boolean exists(Chunk chunk)
 	{
 		File file = new File(chunk.getWorld().getWorldFolder(), "chunk");
