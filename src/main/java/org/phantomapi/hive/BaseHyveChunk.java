1916bbdd577bf1997f008593be8a75ef9911111a
Daniel Mills <danielmillst@gmail.com>
2016-12-24 15:49:02 -0500

    Base chunk jd

--- src/main/java/org/phantomapi/hive/BaseHyveChunk.java
+++ src/main/java/org/phantomapi/hive/BaseHyveChunk.java
@@ -5,6 +5,13 @@ import org.bukkit.Chunk;
 
 public class BaseHyveChunk extends BaseHyve implements HyveChunk
 {
+	/**
+	 * Create an instance of the hyve chunk. Automatically loads it in. Must use
+	 * save() to save any changes
+	 * 
+	 * @param chunk
+	 *            the chunk to load
+	 */
 	public BaseHyveChunk(Chunk chunk)
 	{
 		super(HyveType.CHUNK, "hyve-chunk." + chunk.getX() + "." + chunk.getZ() + ".hy", new File(chunk.getWorld().getWorldFolder(), "chunk"));
