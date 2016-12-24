ac10048aaeff37b2ee06e2f5f7d4d2b70385d469
Daniel Mills <danielmillst@gmail.com>
2016-12-24 15:41:14 -0500

    Add extention and remove world name

--- src/main/java/org/phantomapi/hive/BaseHyveBlock.java
+++ src/main/java/org/phantomapi/hive/BaseHyveBlock.java
@@ -3,11 +3,11 @@ package org.phantomapi.hive;
 import java.io.File;
 import org.bukkit.block.Block;
 
-public class BaseHyveBlock extends BaseHyve
+public class BaseHyveBlock extends BaseHyve implements HyveBlock
 {
 	public BaseHyveBlock(Block block)
 	{
-		super(HyveType.BLOCK, "hyve-block." + block.getWorld().getName() + "." + block.getX() + "." + block.getY() + "." + block.getZ(), new File(block.getWorld().getWorldFolder(), "block"));
+		super(HyveType.BLOCK, "hyve-block." + block.getX() + "." + block.getY() + "." + block.getZ() + ".hy", new File(block.getWorld().getWorldFolder(), "block"));
 		
 		load();
 	}
