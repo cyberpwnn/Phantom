a79da41c6fdd2103352c6269b54e191c0be39412
Daniel Mills <danielmillst@gmail.com>
2016-12-24 15:35:12 -0500

    Hyve base

---
+++ src/main/java/org/phantomapi/hive/Hyve.java
@@ -0,0 +1,27 @@
+package org.phantomapi.hive;
+
+import java.io.File;
+import org.bukkit.block.Block;
+import org.phantomapi.clust.Configurable;
+
+/**
+ * Represents a hyve
+ * 
+ * @author cyberpwn
+ */
+public interface Hyve extends Configurable
+{
+	public HyveType getType();
+	
+	public void save();
+	
+	public void load();
+	
+	public void drop();
+	
+	public static boolean exists(Block block)
+	{
+		File file = new File(block.getWorld().getWorldFolder(), "block");
+		return new File(file, block.getWorld().getName() + "." + block.getX() + "." + block.getY() + "." + block.getZ()).exists();
+	}
+}
