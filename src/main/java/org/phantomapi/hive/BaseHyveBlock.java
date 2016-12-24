78d414c7759f03f9abf20bc6baadb99f326ade6c
Daniel Mills <danielmillst@gmail.com>
2016-12-24 15:49:08 -0500

    Base block jd

--- src/main/java/org/phantomapi/hive/BaseHyveBlock.java
+++ src/main/java/org/phantomapi/hive/BaseHyveBlock.java
@@ -3,8 +3,20 @@ package org.phantomapi.hive;
 import java.io.File;
 import org.bukkit.block.Block;
 
+/**
+ * Base hyve block for a specific block
+ * 
+ * @author cyberpwn
+ */
 public class BaseHyveBlock extends BaseHyve implements HyveBlock
 {
+	/**
+	 * Create and load in the instance of this block. Must invoke save() to save
+	 * any modified changes
+	 * 
+	 * @param block
+	 *            the block to read
+	 */
 	public BaseHyveBlock(Block block)
 	{
 		super(HyveType.BLOCK, "hyve-block." + block.getX() + "." + block.getY() + "." + block.getZ() + ".hy", new File(block.getWorld().getWorldFolder(), "block"));
