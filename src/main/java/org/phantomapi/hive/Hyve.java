1d937f9c34960a7bd8d3e11d50a3a6f6bbf9da9f
Daniel Mills <danielmillst@gmail.com>
2016-12-24 15:43:34 -0500

    Chunk exists

--- src/main/java/org/phantomapi/hive/Hyve.java
+++ src/main/java/org/phantomapi/hive/Hyve.java
@@ -1,6 +1,7 @@
 package org.phantomapi.hive;
 
 import java.io.File;
+import org.bukkit.Chunk;
 import org.bukkit.block.Block;
 import org.phantomapi.clust.Configurable;
 
@@ -23,5 +24,11 @@ public interface Hyve extends Configurable
 	{
 		File file = new File(block.getWorld().getWorldFolder(), "block");
 		return new File(file, "hyve-block." + block.getX() + "." + block.getY() + "." + block.getZ() + ".hy").exists();
+	}
+	
+	public static boolean exists(Chunk chunk)
+	{
+		File file = new File(chunk.getWorld().getWorldFolder(), "chunk");
+		return new File(file, "hyve-chunk." + chunk.getX() + "." + chunk.getZ() + ".hy").exists();
 	}
 }
