06426a5876f95d3be88a73dacc99763b37993206
Daniel Mills <danielmillst@gmail.com>
2017-01-05 15:14:39 -0500

    ctn pack

---
+++ src/main/java/org/phantomapi/ctn/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Represents a "Command Through Network" api for routing custom commands or
+ * functionality through redis (bungeecord is not needed)
+ * 
+ * @author cyberpwn
+ */
+package org.phantomapi.ctn;
