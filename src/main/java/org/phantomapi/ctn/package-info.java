043529db5a607f7f76ef58c87a83b25d1f6f2387
Daniel Mills <danielmillst@gmail.com>
2017-01-07 04:20:32 -0500

    Remove subroutines

--- src/main/java/org/phantomapi/ctn/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Represents a "Command Through Network" api for routing custom commands or
- * functionality through redis (bungeecord is not needed)
- * 
- * @author cyberpwn
- */
-package org.phantomapi.ctn;
