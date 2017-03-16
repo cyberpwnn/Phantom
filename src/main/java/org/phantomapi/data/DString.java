cc1b7ace61d98e9fd7132002931f02d56b803b50
Dan <danielmillst@gmail.com>
2017-03-16 12:02:21 -0400

    DString

---
+++ src/main/java/org/phantomapi/data/DString.java
@@ -0,0 +1,51 @@
+package org.phantomapi.data;
+
+import java.io.IOException;
+
+public class DString implements DataHandle
+{
+	private String string;
+	
+	public DString()
+	{
+		this.string = null;
+	}
+	
+	public DString(String string)
+	{
+		this.string = string;
+	}
+	
+	@Override
+	public byte[] toData() throws IOException
+	{
+		ByteWriter b = new ByteWriter();
+		b.wByte(getDataType());
+		b.wString(string);
+		b.close();
+		
+		return b.getData();
+	}
+
+	@Override
+	public void fromData(byte[] data) throws IOException
+	{
+		ByteReader b = new ByteReader(data);
+		byte ty = b.rByte();
+		
+		if(ty != getDataType())
+		{
+			b.close();
+			return;
+		}
+		
+		string = b.rString();
+		b.close();
+	}
+
+	@Override
+	public byte getDataType()
+	{
+		return 10;
+	}
+}
