9f46deadd0281e2726ace4d34a9e61483efd22e3
Dan <danielmillst@gmail.com>
2017-03-20 04:02:20 -0400

    CMDU

---
+++ src/main/java/org/phantomapi/util/CMD.java
@@ -0,0 +1,79 @@
+package org.phantomapi.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.phantomapi.Phantom;
+
+/**
+ * Quick command utility
+ * 
+ * @author cyberpwn
+ */
+public class CMD
+{
+	private String command;
+	private CommandSender sender;
+	private boolean virtual;
+	
+	/**
+	 * Crate a command
+	 * 
+	 * @param command
+	 *            the command
+	 */
+	public CMD(String command)
+	{
+		this.command = command;
+		sender = Bukkit.getConsoleSender();
+		virtual = false;
+	}
+	
+	/**
+	 * The command should be virtually executed
+	 * 
+	 * @return the chain
+	 */
+	public CMD virtual()
+	{
+		virtual = true;
+		return this;
+	}
+	
+	/**
+	 * Execute the command
+	 * 
+	 * @param sender
+	 *            the sender as the runner
+	 */
+	public void execute(CommandSender sender)
+	{
+		this.sender = sender;
+		execute();
+	}
+	
+	/**
+	 * Execute the command as the console
+	 */
+	public void execute()
+	{
+		if(virtual)
+		{
+			if(sender instanceof Player)
+			{
+				Phantom.instance().callEvent(new PlayerCommandPreprocessEvent((Player) sender, command.startsWith("/") ? command : "/" + command));
+			}
+			
+			else
+			{
+				Bukkit.dispatchCommand(sender, "phast c " + (command.startsWith("/") ? command.substring(1) : command) + ";");
+			}
+		}
+		
+		else
+		{
+			Bukkit.dispatchCommand(sender, command);
+		}
+	}
+}
