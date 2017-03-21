7b52fba68b1eaf672e174b4f78e08738b33d887b
Dan <danielmillst@gmail.com>
2017-03-21 02:11:01 -0400

    New brush

---
+++ src/main/java/org/phantomapi/builder/Brush.java
@@ -0,0 +1,9 @@
+package org.phantomapi.builder;
+
+import org.bukkit.Location;
+import org.phantomapi.world.VariableBlock;
+
+public interface Brush
+{
+	public VariableBlock brush(Location location);
+}
