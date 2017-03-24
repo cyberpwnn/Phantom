21de895f4c8b08d4d1cd591e038e270a24d1c0fc
Dan <danielmillst@gmail.com>
2017-03-24 19:09:14 -0400

    Handle npcs

---
+++ src/main/java/org/phantomapi/core/WraithController.java
@@ -0,0 +1,37 @@
+package org.phantomapi.core;
+
+import org.bukkit.entity.Entity;
+import org.phantomapi.construct.Controllable;
+import org.phantomapi.construct.Controller;
+import org.phantomapi.wraith.WU;
+import net.citizensnpcs.api.CitizensAPI;
+
+public class WraithController extends Controller
+{
+	public WraithController(Controllable parentController)
+	{
+		super(parentController);
+	}
+	
+	@Override
+	public void onStart()
+	{
+		destroyNPCs();
+	}
+	
+	@Override
+	public void onStop()
+	{
+		
+	}
+	
+	public void destroyNPCs()
+	{
+		for(Entity i : WU.getNPCs())
+		{
+			CitizensAPI.getNPCRegistry().getNPC(i).destroy();
+		}
+		
+		CitizensAPI.getNPCRegistry().deregisterAll();
+	}
+}
