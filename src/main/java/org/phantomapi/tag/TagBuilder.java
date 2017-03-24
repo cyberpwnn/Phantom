4dde395f6eb665b3c8c10fe5f5a1aeb85fb7b1bd
Dan <danielmillst@gmail.com>
2017-03-23 22:17:13 -0400

    Dont hide the first one

--- src/main/java/org/phantomapi/tag/TagBuilder.java
+++ src/main/java/org/phantomapi/tag/TagBuilder.java
@@ -78,19 +78,6 @@ public class TagBuilder
 			{
 				NMSX.hideEntity((Player) base, locks.get(i));
 			}
-			
-			try
-			{
-				if(context.get(context.size() - 1).equals(i))
-				{
-					NMSX.hideEntity(locks.get(i));
-				}
-			}
-			
-			catch(Exception e)
-			{
-				
-			}
 		}
 	}
 	
