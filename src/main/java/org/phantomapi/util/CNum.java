4be53626be042c46931e8ba845ad6f6fe9bdc9d8
Dan <danielmillst@gmail.com>
2017-03-25 04:22:25 -0400

    Circle numbers

---
+++ src/main/java/org/phantomapi/util/CNum.java
@@ -0,0 +1,44 @@
+package org.phantomapi.util;
+
+public class CNum
+{
+	private int number;
+	private int max;
+	
+	public CNum(int max)
+	{
+		number = 0;
+		this.max = max;
+	}
+	
+	public CNum set(int n)
+	{
+		number = n;
+		circ();
+		return this;
+	}
+	
+	public CNum add(int a)
+	{
+		number += a;
+		circ();
+		return this;
+	}
+	
+	public CNum sub(int a)
+	{
+		number -= a;
+		circ();
+		return this;
+	}
+	
+	public int get()
+	{
+		return number;
+	}
+	
+	public void circ()
+	{
+		number = number % (max);
+	}
+}
