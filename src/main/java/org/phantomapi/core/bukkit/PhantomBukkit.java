bb10e2e1131696a59b663306e752e5385df1d03e
Dan <cyberpwnn@users.noreply.github.com>
2017-11-18 18:34:10 -0500

    PhantomBukkit instance

--- src/main/java/org/phantomapi/core/bukkit/PhantomBukkit.java
+++ src/main/java/org/phantomapi/core/bukkit/PhantomBukkit.java
@@ -1,6 +1,45 @@
 package org.phantomapi.core.bukkit;
 
-public class PhantomBukkit
-{
+import java.io.File;
 
+import org.cyberpwn.glog.L;
+import org.phantomapi.core.CorePlugin;
+
+public class PhantomBukkit extends BukkitPlugin
+{
+	public CorePlugin core;
+
+	@Override
+	public void onInit()
+	{
+		try
+		{
+			core = new CorePlugin(new File("plugins"));
+			core.onInit();
+		}
+
+		catch(Throwable e)
+		{
+			L.l("Failed to start phantom core");
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onLoad()
+	{
+		core.onLoad();
+	}
+
+	@Override
+	public void onEnable()
+	{
+		core.onEnable();
+	}
+
+	@Override
+	public void onDisable()
+	{
+		core.onDisable();
+	}
 }
