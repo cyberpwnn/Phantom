6a03829bc88e27eb45c1b2df7469d7694d6bcec0
Dan <cyberpwnn@users.noreply.github.com>
2018-01-02 03:18:12 -0500

    Pawn interface

---
+++ src/main/java/phantom/pawn/IPawn.java
@@ -0,0 +1,13 @@
+package phantom.pawn;
+
+import org.bukkit.event.Listener;
+
+/**
+ * Represents a controlled object which is exposed to phantom
+ *
+ * @author cyberpwn
+ */
+public interface IPawn extends Listener
+{
+
+}
