2ef612a351a2a8ce42e599b1e98bfa3cbf207f33
cyberpwn <danielmillst@gmail.com>
2018-01-03 01:12:14 -0500

    GQueue

---
+++ src/main/java/phantom/lang/GQueue.java
@@ -0,0 +1,8 @@
+package phantom.lang;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class GQueue<T> extends ConcurrentLinkedQueue<T>
+{
+	private static final long serialVersionUID = 3174608403073474495L;
+}
