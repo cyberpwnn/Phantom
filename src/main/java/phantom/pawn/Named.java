57440ca84cf3489a77f0567bbcdf75ff389f16d9
cyberpwn <danielmillst@gmail.com>
2018-01-03 01:27:15 -0500

    Pawn names closes #110

---
+++ src/main/java/phantom/pawn/Named.java
@@ -0,0 +1,13 @@
+package phantom.pawn;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface Named
+{
+	String value();
+}
