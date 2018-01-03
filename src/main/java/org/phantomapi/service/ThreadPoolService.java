ffa531632452a2bf0f1d68a01fa56685a0b53620
cyberpwn <danielmillst@gmail.com>
2018-01-03 02:19:27 -0500

    Thread pool service

---
+++ src/main/java/org/phantomapi/service/ThreadPoolService.java
@@ -0,0 +1,24 @@
+package org.phantomapi.service;
+
+import phantom.pawn.Name;
+import phantom.pawn.Singular;
+import phantom.pawn.Start;
+import phantom.pawn.Stop;
+import phantom.service.IService;
+
+@Name("SVC Thread Pool")
+@Singular
+public class ThreadPoolService implements IService
+{
+	@Start
+	public void start()
+	{
+		
+	}
+	
+	@Stop
+	public void stop()
+	{
+		
+	}
+}
