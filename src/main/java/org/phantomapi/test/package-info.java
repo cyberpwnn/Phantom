69a44b360d2424363dbea4122230b257654bfec8
cyberpwn <danielmillst@gmail.com>
2018-01-04 05:23:57 -0500

    PAckage docs

---
+++ src/main/java/org/phantomapi/test/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * A collection tests
+ *
+ * @author cyberpwn
+ */
+@Documented
+package org.phantomapi.test;
+
+import phantom.util.metrics.Documented;
---
+++ src/main/java/phantom/dispatch/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * A collection dispatch apis
+ *
+ * @author cyberpwn
+ */
+@Documented
+package phantom.dispatch;
+
+import phantom.util.metrics.Documented;
