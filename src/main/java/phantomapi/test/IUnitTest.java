59e43618ab8a5c36ca1213d87593aab7188c1f63
cyberpwn <danielmillst@gmail.com>
2018-01-04 04:45:05 -0500

    Unit test pawn

---
+++ src/main/java/phantomapi/test/IUnitTest.java
@@ -0,0 +1,13 @@
+package phantomapi.test;
+
+import phantom.lang.Callback;
+import phantom.pawn.IPawn;
+
+public interface IUnitTest extends IPawn
+{
+	public String[] getTestNames();
+
+	public void test(Callback<TestResult> callbackSet, String[] args);
+
+	public void test(Callback<TestResult> callbackSet);
+}
