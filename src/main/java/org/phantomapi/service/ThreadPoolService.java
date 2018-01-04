19c77f67d72dad2eeb5bc47afc3c56484cbef582
Dan <cyberpwnn@users.noreply.github.com>
2018-01-04 03:19:36 -0500

    Pool service

--- src/main/java/org/phantomapi/service/ThreadPoolService.java
+++ src/main/java/org/phantomapi/service/ThreadPoolService.java
@@ -4,21 +4,38 @@ import phantom.pawn.Name;
 import phantom.pawn.Singular;
 import phantom.pawn.Start;
 import phantom.pawn.Stop;
+import phantom.sched.A;
+import phantom.sched.ParallelPoolManager;
+import phantom.sched.QueueMode;
 import phantom.service.IService;
 
 @Name("SVC Thread Pool")
 @Singular
 public class ThreadPoolService implements IService
 {
+	private ParallelPoolManager pool;
+
 	@Start
 	public void start()
 	{
-		
+		pool = new ParallelPoolManager("Phantom Ghost", 2, QueueMode.ROUND_ROBIN)
+		{
+			@Override
+			public long getNanoGate()
+			{
+				return 1000000;
+			}
+		};
 	}
-	
+
 	@Stop
 	public void stop()
 	{
-		
+		pool.shutdown();
+	}
+
+	public void run(A a)
+	{
+		pool.queue(a);
 	}
 }
