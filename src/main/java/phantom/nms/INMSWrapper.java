e0b1e59765e5f6ce4665fd3dcc35949b7e56606d
cyberpwn <danielmillst@gmail.com>
2018-01-04 09:06:56 -0500

    IWrapper

--- src/main/java/phantom/nms/INMSWrapper.java
+++ src/main/java/phantom/nms/INMSWrapper.java
@@ -1,6 +1,36 @@
 package phantom.nms;
 
-public interface INMSWrapper
+import phantom.net.Protocol;
+import phantom.net.ProtocolRange;
+import phantom.pawn.IPawn;
+import phantom.util.metrics.Documented;
+
+/**
+ * Basic nms wrapper
+ *
+ * @author cyberpwn
+ */
+@Documented
+public interface INMSWrapper extends IPawn
 {
+	/**
+	 * Get the target protocol
+	 *
+	 * @return the target
+	 */
+	public Protocol getTargetProtocol();
+
+	/**
+	 * The protocol range
+	 *
+	 * @return the range
+	 */
+	public ProtocolRange getProtocolRange();
+
+	/**
+	 * The package version
+	 *
+	 * @return a string representing the package version
+	 */
 	public String getPackageVersion();
 }
