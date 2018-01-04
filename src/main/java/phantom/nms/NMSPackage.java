1888c6d25470ae8cb73bc082624b4d50e2674174
cyberpwn <danielmillst@gmail.com>
2018-01-04 09:06:45 -0500

    NMS Package

---
+++ src/main/java/phantom/nms/NMSPackage.java
@@ -0,0 +1,14 @@
+package phantom.nms;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface NMSPackage
+{
+	String value();
+}
