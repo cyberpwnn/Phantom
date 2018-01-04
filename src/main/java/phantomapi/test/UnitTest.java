343f147f8b7fd3b0b058dd1d5cf10730e643cc26
cyberpwn <danielmillst@gmail.com>
2018-01-04 04:44:54 -0500

    Unit test params

---
+++ src/main/java/phantomapi/test/UnitTest.java
@@ -0,0 +1,14 @@
+package phantomapi.test;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface UnitTest
+{
+	String[] value();
+}
