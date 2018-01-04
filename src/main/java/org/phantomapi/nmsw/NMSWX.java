0eb26c51396de9e844fce747452e1bb22e4aef13
cyberpwn <danielmillst@gmail.com>
2018-01-04 09:07:17 -0500

    NMS Unknown

---
+++ src/main/java/org/phantomapi/nmsw/NMSWX.java
@@ -0,0 +1,42 @@
+package org.phantomapi.nmsw;
+
+import phantom.net.Protocol;
+import phantom.nms.NMSPackage;
+import phantom.nms.NMSWrapper;
+import phantom.pawn.Name;
+import phantom.pawn.Singular;
+import phantom.pawn.Start;
+import phantom.pawn.Stop;
+import phantom.util.metrics.Anchor;
+import phantom.util.metrics.Documented;
+
+/**
+ * Empty NMS Wrapper<br/>
+ * > Supports Nothing
+ *
+ * @author cyberpwn
+ */
+@Documented
+@Singular
+@Anchor("phantom-nmsw")
+@Name("NMS Wrapper Unsupported")
+@NMSPackage("UNKNOWN")
+public class NMSWX extends NMSWrapper
+{
+	public NMSWX()
+	{
+		super(Protocol.EARLIEST.to(Protocol.R1_7_10));
+	}
+
+	@Start
+	public void onStart()
+	{
+
+	}
+
+	@Stop
+	public void onStop()
+	{
+
+	}
+}
