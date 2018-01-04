1b737182405254aaca7343a3a7f441da8a396660
cyberpwn <danielmillst@gmail.com>
2018-01-04 09:07:28 -0500

    nms 18

---
+++ src/main/java/org/phantomapi/nmsw/NMSW8.java
@@ -0,0 +1,51 @@
+package org.phantomapi.nmsw;
+
+import phantom.net.Protocol;
+import phantom.nms.NMSPackage;
+import phantom.nms.NMSWrapper;
+import phantom.pawn.Name;
+import phantom.pawn.Singular;
+import phantom.pawn.Start;
+import phantom.pawn.Stop;
+import phantom.util.metrics.Anchor;
+import phantom.util.metrics.Documented;
+
+/**
+ * 1.8.X NMS Wrapper<br/>
+ * > Supports 1.8.9<br/>
+ * > Supports 1.8.8<br/>
+ * > Supports 1.8.7<br/>
+ * > Supports 1.8.6<br/>
+ * > Supports 1.8.5<br/>
+ * > Supports 1.8.4<br/>
+ * > Supports 1.8.3<br/>
+ * > Supports 1.8.2<br/>
+ * > Supports 1.8.1<br/>
+ * > Supports 1.8
+ *
+ * @author cyberpwn
+ */
+@Documented
+@Singular
+@Anchor("phantom-nmsw")
+@Name("NMS Wrapper 1.8.X")
+@NMSPackage("v1_8_R3")
+public class NMSW8 extends NMSWrapper
+{
+	public NMSW8()
+	{
+		super(Protocol.R1_8.to(Protocol.R1_8_9));
+	}
+
+	@Start
+	public void onStart()
+	{
+
+	}
+
+	@Stop
+	public void onStop()
+	{
+
+	}
+}
