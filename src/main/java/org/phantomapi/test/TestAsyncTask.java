3b2358143dfa95dc9cc1a034efb30770a32825c7
cyberpwn <danielmillst@gmail.com>
2018-01-05 05:36:50 -0500

    Enhance unit tests as commands

--- src/main/java/org/phantomapi/test/TestAsyncTask.java
+++ src/main/java/org/phantomapi/test/TestAsyncTask.java
@@ -1,16 +1,22 @@
 package org.phantomapi.test;
 
+import org.bukkit.command.CommandSender;
 import org.phantomapi.Phantom;
 import org.phantomapi.service.ThreadPoolService;
 
+import phantom.command.PhantomCommand;
 import phantom.lang.Callback;
 import phantom.lang.FinalInteger;
 import phantom.pawn.Name;
 import phantom.pawn.Singular;
 import phantom.sched.A;
+import phantom.sched.S;
 import phantom.test.IUnitTest;
 import phantom.test.TestResult;
 import phantom.test.UnitTest;
+import phantom.text.C;
+import phantom.text.ITagProvider;
+import phantom.text.TXT;
 import phantom.util.metrics.Documented;
 
 /**
@@ -22,8 +28,16 @@ import phantom.util.metrics.Documented;
 @Singular
 @Name("TEST Async Task")
 @UnitTest({"async", "async-task"})
-public class TestAsyncTask implements IUnitTest
+public class TestAsyncTask extends PhantomCommand implements IUnitTest
 {
+	public TestAsyncTask()
+	{
+		super("async");
+		addAlias("async-task");
+		addAlias("asy");
+		setPrameterUsage("[i:task size]");
+	}
+
 	@Override
 	public String[] getTestNames()
 	{
@@ -45,12 +59,14 @@ public class TestAsyncTask implements IUnitTest
 
 			catch(NumberFormatException e)
 			{
-				callbackSet.run(new TestResult("Invalid integer argument: " + args[0]));
+				callbackSet.run(new TestResult("Invalid integer argument: " + args[0], true));
 				return;
 			}
 		}
 
-		for(int i = 0; i < 100; i++)
+		callbackSet.run(new TestResult("Queued " + goal + " tasks to be run."));
+
+		for(int i = 0; i < goal; i++)
 		{
 			new A()
 			{
@@ -63,13 +79,48 @@ public class TestAsyncTask implements IUnitTest
 		}
 
 		Phantom.getService(ThreadPoolService.class).lock();
-		callbackSet.run(new TestResult("Executed " + fi.get() + " of " + goal + " tasks", fi.get() != goal));
+
+		int gg = goal;
+
+		new S(20)
+		{
+			@Override
+			public void run()
+			{
+				TestResult tr = new TestResult("Executed " + fi.get() + " of " + gg + " tasks in 20 ticks", fi.get() != gg);
+				tr.setFinisher(true);
+				callbackSet.run(tr);
+			}
+		};
 	}
 
 	@Override
 	public void test(Callback<TestResult> callbackSet)
 	{
 		test(callbackSet, new String[0]);
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, String[] a)
+	{
+		ITagProvider tag = this;
+
+		test(new Callback<TestResult>()
+		{
+			@Override
+			public void run()
+			{
+				get().sendResults(sender, tag);
+			}
+		}, a);
+
+		return true;
+	}
+
+	@Override
+	public String getTag(CommandSender sender)
+	{
+		return TXT.makeTag(C.LIGHT_PURPLE, C.DARK_GRAY, C.LIGHT_PURPLE, C.GRAY, "Phantom" + C.WHITE + " Test");
 	}
 
 }
