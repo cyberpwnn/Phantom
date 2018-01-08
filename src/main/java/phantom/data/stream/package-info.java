548725289c9d40c29d45870381a18503a1e0d0f3
cyberpwn <danielmillst@gmail.com>
2018-01-08 01:08:11 -0500

    pkg info

---
+++ src/main/java/phantom/data/stream/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * A collection of data streams
+ *
+ * @author cyberpwn
+ */
+@Documented
+package phantom.data.stream;
+
+import phantom.util.metrics.Documented;
---
+++ src/main/java/phantom/util/data/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * A collection of data utils
+ *
+ * @author cyberpwn
+ */
+@Documented
+package phantom.util.data;
+
+import phantom.util.metrics.Documented;
