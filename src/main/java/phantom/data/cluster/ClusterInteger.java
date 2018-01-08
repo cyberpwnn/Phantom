a821dfe2c864fefbbbf4057c79226711fc4bab14
cyberpwn <danielmillst@gmail.com>
2018-01-08 01:09:10 -0500

    Generic clusters

---
+++ src/main/java/phantom/data/cluster/ClusterInteger.java
@@ -0,0 +1,40 @@
+package phantom.data.cluster;
+
+public class ClusterInteger extends PhantomCluster<Integer>
+{
+	public ClusterInteger(Integer value)
+	{
+		super(value);
+	}
+
+	@Override
+	public void fromBytes(byte[] data)
+	{
+		set(((data[0] << 24) + (data[1] << 16) + (data[2] << 8) + (data[3] << 0)));
+	}
+
+	@Override
+	public byte[] toBytes()
+	{
+		int v = get();
+		byte[] buf = new byte[4];
+		buf[0] = (byte) ((v >>> 24) & 0xFF);
+		buf[1] = (byte) ((v >>> 16) & 0xFF);
+		buf[2] = (byte) ((v >>> 8) & 0xFF);
+		buf[3] = (byte) ((v >>> 0) & 0xFF);
+
+		return buf;
+	}
+
+	@Override
+	public String asString()
+	{
+		return get().toString();
+	}
+
+	@Override
+	public void fromString(String s)
+	{
+		set(Integer.valueOf(s));
+	}
+}
