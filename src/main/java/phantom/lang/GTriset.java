35e158d05e6c1fdededb19a6eaa16da5e35d2f1b
cyberpwn <danielmillst@gmail.com>
2018-01-10 05:20:09 -0500

    Multiset equals

--- src/main/java/phantom/lang/GTriset.java
+++ src/main/java/phantom/lang/GTriset.java
@@ -45,4 +45,49 @@ public class GTriset<A, B, C> implements Serializable
 	{
 		this.c = c;
 	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((a == null) ? 0 : a.hashCode());
+		result = prime * result + ((b == null) ? 0 : b.hashCode());
+		result = prime * result + ((c == null) ? 0 : c.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(this == obj)
+			return true;
+		if(obj == null)
+			return false;
+		if(getClass() != obj.getClass())
+			return false;
+		GTriset<?, ?, ?> other = (GTriset<?, ?, ?>) obj;
+		if(a == null)
+		{
+			if(other.a != null)
+				return false;
+		}
+		else if(!a.equals(other.a))
+			return false;
+		if(b == null)
+		{
+			if(other.b != null)
+				return false;
+		}
+		else if(!b.equals(other.b))
+			return false;
+		if(c == null)
+		{
+			if(other.c != null)
+				return false;
+		}
+		else if(!c.equals(other.c))
+			return false;
+		return true;
+	}
 }
